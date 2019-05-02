57f22efdeb31346980e3a60a867debe4e4569a9d
John Kakareka <kakareka@nih.gov>
2019-05-02 16:00:41 -0400

    code cleanup to SSC driver

--- Code/PressureSensor/PressureSensor/main.c
+++ Code/PressureSensor/PressureSensor/main.c
@@ -2,8 +2,7 @@
 #include <stdio.h>
 #include "SSC_I2C_Pressure.h"
 
-bool g_newread = true;
-uint8_t msb, lsb;
+
 
 int main(void)
 {
@@ -13,11 +12,11 @@ int main(void)
     ssc_init();
 
     while (1) {
-        g_newread = true;
+
         ssc_start_read();
         __bis_SR_register(LPM0_bits + GIE);
 
-        psi = convert_to_psi(msb, lsb);
+        psi = ssc_get_last_psi();
         printf("psi - %d\n", psi);
     }
 
@@ -25,27 +24,3 @@ int main(void)
 }
 
 
-#if defined(__TI_COMPILER_VERSION__) || defined(__IAR_SYSTEMS_ICC__)
-#pragma vector=USCI_B1_VECTOR
-__interrupt
-#elif defined(__GNUC__)
-__attribute__((interrupt(USCI_B1_VECTOR)))
-#endif
-void USCI_B1_ISR (void)
-{
-    switch (__even_in_range(UCB1IV,12)){
-        case USCI_I2C_UCRXIFG:
-        {
-            if (g_newread) {
-                msb = UCB1RXBUF;
-                g_newread = false;
-                UCB1CTL1 |= UCTXSTP;  //issue stop
-            } else {
-                lsb = UCB1RXBUF;
-                __bic_SR_register_on_exit(LPM0_bits);
-
-            }
-            break;
-        }
-    }
-}
--- Code/PressureSensor/lib/SSC_I2C_Pressure.c
+++ Code/PressureSensor/lib/SSC_I2C_Pressure.c
@@ -1,6 +1,8 @@
 #include "SSC_I2C_Pressure.h"
+#include "driverlib.h"
 
-unsigned char bytes_to_read = 0;
+#define SENSOR_ADDRESS 0x28
+#define I2C_BASE USCI_B1_BASE
 
 // JWK, min/max from datasheet and Honeywell I2C (10% calibration)
 // JWK assume part SSCDANT030PG2A3
@@ -12,7 +14,10 @@ const uint16_t SENSOR_MIN_PSI = 0;
 // 0-30PSI, 10% - 90% 14 bit sensor (30 - 0) / (0x3999 - 0x0666)
 const float SENSOR_RATIO = 0.0022888532845044634;
 
+bool g_newread = true;
+uint8_t msb, lsb;
 uint16_t last_psi;
+uint16_t convert_to_psi(uint8_t msb, uint8_t lsb);
 
 void ssc_init(void)
 {
@@ -41,27 +46,10 @@ void ssc_init(void)
 
 void ssc_start_read(void)
 {
-    bytes_to_read = BYTES_PER_READING;
+    last_psi = convert_to_psi(msb, lsb);
+    g_newread = true;
 
     USCI_B_I2C_masterReceiveMultiByteStart(I2C_BASE);
-}
-
-bool ssc_process_byte(void)
-{
-    bool reading_complete = false;
-    bytes_to_read--;
-
-    if (bytes_to_read == 1) {
-        //msb = USCI_B_I2C_masterReceiveMultiByteNext(I2C_BASE);
-    }
-    else {
-        //lsb = USCI_B_I2C_masterReceiveMultiByteFinish(I2C_BASE);
-        reading_complete = true;
-
-    }
-
-    return reading_complete;
-
 }
 
 uint16_t convert_to_psi(uint8_t msb, uint8_t lsb)
@@ -79,4 +67,28 @@ uint16_t convert_to_psi(uint8_t msb, uint8_t lsb)
 int ssc_get_last_psi(void)
 {
     return last_psi;
+}
+
+#if defined(__TI_COMPILER_VERSION__) || defined(__IAR_SYSTEMS_ICC__)
+#pragma vector=USCI_B1_VECTOR
+__interrupt
+#elif defined(__GNUC__)
+__attribute__((interrupt(USCI_B1_VECTOR)))
+#endif
+void USCI_B1_ISR (void)
+{
+    switch (__even_in_range(UCB1IV,12)){
+        case USCI_I2C_UCRXIFG:
+        {
+            if (g_newread) {
+                msb = USCI_B_I2C_masterReceiveMultiByteFinish(I2C_BASE);
+                g_newread = false;
+            } else {
+                lsb = USCI_B_I2C_masterReceiveMultiByteNext(I2C_BASE);
+                __bic_SR_register_on_exit(LPM0_bits);
+
+            }
+            break;
+        }
+    }
 }
