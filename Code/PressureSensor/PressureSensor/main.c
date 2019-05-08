9ae024256dc51bd07257826cd5d8071eb5fcbcae
John W Kakareka <kakareka@nih.gov>
2019-05-08 12:21:52 -0400

    Update I2C test program to explicitly set clocks to verify the needed clocks for the TFT/LCD are compatible with the I2C

--- Code/PressureSensor/PressureSensor/main.c
+++ Code/PressureSensor/PressureSensor/main.c
@@ -9,6 +9,28 @@ int main(void)
     int psi = 0;
     WDT_A_hold(WDT_A_BASE);
 
+
+    GPIO_setAsPeripheralModuleFunctionInputPin(
+            GPIO_PORT_P5, GPIO_PIN2 + GPIO_PIN3 +
+            GPIO_PIN4 + GPIO_PIN5);
+
+    UCS_setExternalClockSource(32768,4000000);
+
+    // Set Vcore to accomodate for max. allowed system speed
+    PMM_setVCore(PMM_CORE_LEVEL_3);
+
+    // Use 32.768kHz XTAL as reference
+    UCS_turnOnLFXT1(UCS_XT1_DRIVE_3, UCS_XCAP_3);
+
+    UCS_turnOnXT2(UCS_XT2_DRIVE_4MHZ_8MHZ);
+
+    // Set system clock to max (25MHz)
+    UCS_initFLLSettle(25000, 762);
+
+    UCS_initClockSignal(UCS_SMCLK, UCS_DCOCLK_SELECT, UCS_CLOCK_DIVIDER_16);
+
+    SFR_enableInterrupt(SFR_OSCILLATOR_FAULT_INTERRUPT);
+
     ssc_init();
 
     while (1) {
