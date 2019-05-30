b1c663b05f31e035bb66eb8e8f43ed22d3438625
John Kakareka <kakareka@nih.gov>
2019-05-30 13:56:23 -0400

    After all poins checked, remove uproc from LPM state if port is even, shift return of GPIO_getInterruptStatus by 8 to reverse shifting down internally by function Use #define for length of ports/pins vector. Was done initally for debugging, but avoid need to use sizeof in loop

--- Code/PressureSensor/lib/Switches.c
+++ Code/PressureSensor/lib/Switches.c
@@ -6,9 +6,10 @@
 #define SW_TRANSITION GPIO_HIGH_TO_LOW_TRANSITION
 
 // IRQ handlers need to be updated manually if Port changes
-const uint8_t sw_ports[6] = {GPIO_PORT_P1, GPIO_PORT_P1, GPIO_PORT_P2,
+#define TOTAL_GPIO 6
+const uint8_t sw_ports[] = {GPIO_PORT_P1, GPIO_PORT_P1, GPIO_PORT_P2,
                              GPIO_PORT_P2, GPIO_PORT_P2, GPIO_PORT_P2};
-const uint16_t sw_pins[6] = {GPIO_PIN3, GPIO_PIN2, GPIO_PIN3,
+const uint16_t sw_pins[] = {GPIO_PIN3, GPIO_PIN2, GPIO_PIN3,
                             GPIO_PIN4, GPIO_PIN0, GPIO_PIN2};
 
 bool sw_status[6];
@@ -24,16 +25,23 @@ static void _setup_switch(uint8_t port, uint16_t pin)
 
 void sw_init(void)
 {
-    for (int i=0;i<sizeof(sw_status);i++) {
+
+    for (int i=0;i<TOTAL_GPIO;i++) {
         sw_status[i] = false;
         _setup_switch(sw_ports[i], sw_pins[i]);
     }
+
 }
 
 static inline void _check_and_ack_sw(uint8_t sw_id)
 {
     uint16_t status;
     status = GPIO_getInterruptStatus(sw_ports[sw_id], sw_pins[sw_id]);
+    // GPIO_getInterruptStatus shifts even ports by 8
+    if((sw_ports[sw_id] & 1) ^ 1)
+    {
+        status >>= 8;
+    }
     if (status & sw_pins[sw_id]) {
         sw_status[sw_id] = true;
         GPIO_clearInterrupt(sw_ports[sw_id], sw_pins[sw_id]);
@@ -51,6 +59,7 @@ void Port_1 (void)
 {
     _check_and_ack_sw(SW_UL);
     _check_and_ack_sw(SW_ML);
+    __bic_SR_register_on_exit(LPM0_bits);
 }
 
 #if defined(__TI_COMPILER_VERSION__) || defined(__IAR_SYSTEMS_ICC__)
@@ -65,4 +74,5 @@ void Port_2 (void)
     _check_and_ack_sw(SW_MR);
     _check_and_ack_sw(SW_LR);
     _check_and_ack_sw(SW_LL);
+    __bic_SR_register_on_exit(LPM0_bits);
 }
