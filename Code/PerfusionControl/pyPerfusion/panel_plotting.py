a6352e28f5f34c4f10033b77f0e0da566432dec0
John W Kakareka <kakareka@nih.gov>
2020-08-21 13:55:21 -0400

    update plotting panel to switch between different sensors and plot dummy data for testing

--- Code/PerfusionControl/pyPerfusion/panel_plotting.py
+++ Code/PerfusionControl/pyPerfusion/panel_plotting.py
@@ -6,53 +6,56 @@
 Panel class for plotting data
 """
 import wx
-from enum import Enum
 
 import numpy as np
 import matplotlib as mpl
-from cycler import cycler
 from matplotlib.backends.backend_wxagg import FigureCanvasWxAgg
-from matplotlib.backends.backend_wx import NavigationToolbar2Wx
+# from matplotlib.backends.backend_wx import NavigationToolbar2Wx
 
 
 class DummyPerfusionSensors:
-    ha_flow = [1] * 100
-    pv_flow = [4] * 100
+    time = {}
+    data = {}
+
+    time['HA Flow'] = np.arange(0, 10, 0.001)
+    data['HA Flow'] = np.sin(2 * np.pi * time['HA Flow'])
+
+    time['PV Flow'] = np.arange(0, 10, 0.001)
+    data['PV Flow'] = np.sin(0.1 * 2 * np.pi * time['PV Flow'])
 
 
 class PanelPlotting(wx.Panel):
     def __init__(self, parent, perfusion_sensors):
         self.parent = parent
         self._sensors = perfusion_sensors
+        self.sensor_name = ''
         wx.Panel.__init__(self, parent, -1)
 
         self.fig = mpl.figure.Figure()
         self.canvas = FigureCanvasWxAgg(self, wx.ID_ANY, self.fig)
         self.canvas.SetMinSize(wx.Size(1, 1))
         self.fig.tight_layout()
-        self.plot_len = 1000
-        self.lines = [None] * 8
-        self.colors = ['red', 'green', 'blue', 'black',
-                       'magenta', 'orange', 'brown', 'cyan', 'black',
-                       'blue', 'red']
-        self.linestyles = ['-', '-.']
 
         # self.toolbar = NavigationToolbar2Wx(self.canvas)
         # self.toolbar.Realize()
 
         self.sizer = wx.BoxSizer(wx.VERTICAL)
 
-        parameters = ["HA Flow", "PV Flow", "HA Pressure", "PV Pressure"]
+        parameters = list(self._sensors.time.keys())
         self.choice_plot_parameter = wx.Choice(self, choices=parameters)
         self.choice_plot_parameter.SetSelection(1)
+        self.sensor_name = parameters[1]
         font = self.choice_plot_parameter.GetFont()
         font.SetPointSize(20)
         self.choice_plot_parameter.SetFont(font)
 
         self.__do_layout()
-        # self.__set_bindings()
+        self.__set_bindings()
         self.axes = self.fig.add_subplot(111)
-        self.setup_plotlines()
+
+        self.timer_plot = wx.Timer(self)
+        self.Bind(wx.EVT_TIMER, self.OnTimer)
+        self.timer_plot.Start(100, wx.TIMER_CONTINUOUS)
 
     def __do_layout(self):
 
@@ -64,22 +67,24 @@ class PanelPlotting(wx.Panel):
         self.Fit()
         self.Layout()
 
-    # def __set_bindings(self):
-        # set bindings if needed
+    def __set_bindings(self):
+        self.choice_plot_parameter.Bind(wx.EVT_CHOICE, self.OnPlotChoiceChange)
 
-    def setup_plotlines(self):
-        self.axes.clear()
-        self.lines = [[None] * 2 for i in range(11)]
-        for i in range(11):
-            for j in range(2):
-                line = self.axes.plot([0]*self.plot_len,
-                                      linestyle=self.linestyles[j],
-                                      color=self.colors[i],
-                                      visible=False)[0]
-                self.lines[i][j] = line
+    def OnPlotChoiceChange(self, event):
+        new_name = self.choice_plot_parameter.GetStringSelection()
+        print(f'{new_name}')
+        if not new_name == self.sensor_name:
+            self.sensor_name = new_name
+            self.axes.clear()
 
-    def plot_pos(self, pos, sector, data):
+    def plot(self, sensor_name):
+        self.axes.plot(self._sensors.time[sensor_name], self._sensors.data[sensor_name], color='green')
         self.canvas.draw()
+
+    def OnTimer(self, event):
+        if event.GetId() == self.timer_plot.GetId():
+            self.plot(self.sensor_name)
+
 
 
 class TestFrame(wx.Frame):
