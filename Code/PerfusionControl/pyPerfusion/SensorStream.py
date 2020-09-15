ff35ce229c2798348b5c623408529b0945506706
John W Kakareka <kakareka@nih.gov>
2020-09-15 15:02:05 -0400

    re-add printing of header small code cleanups

--- Code/PerfusionControl/pyPerfusion/SensorStream.py
+++ Code/PerfusionControl/pyPerfusion/SensorStream.py
@@ -2,6 +2,7 @@ import pathlib
 import datetime
 from threading import Thread, Event
 from os import SEEK_END
+import time
 
 import numpy as np
 
@@ -32,7 +33,6 @@ class SensorStream(Thread):
             data_buf = self.__hw.get_data()
             if data_buf is not None and self.__fid is not None:
                 buf_len = len(data_buf)
-                # self.__fid.write(data_buf.tobytes())
                 data_buf.tofile(self.__fid)
                 self.__last_idx += buf_len
                 self.__fid.flush()
@@ -53,7 +53,6 @@ class SensorStream(Thread):
         self.__timestamp = datetime.datetime.now()
         self.print_header()
 
-
     def stop(self):
         self.__hw.halt()
         self.__evt_halt.set()
@@ -67,39 +66,35 @@ class SensorStream(Thread):
             self.__fid.close()
             self.__fid = None
         full_path = self._project_path / self._study_path / self._filename
-        # self.__fid = open(full_path, 'wt')
-        # stamp_str = self.__timestamp.strftime('%Y-%m-%d_%H:%M')
-        #
-        # print(f'Data Format: {DATA_VERSION}', file=self.__fid)
-        # print(f'Sensor: {self.__name}', file=self.__fid)
-        # print(f'Unit: {self.__unit_str}', file=self.__fid)
-        # print(f'Data Format: float32', file=self.__fid)
-        # print(f'Start of Acquisition: {stamp_str}', file=self.__fid)
-        # self.__end_of_header = self.__fid.tell()
-        # print(f'end of header is {self.__end_of_header}')
-        # self.__fid.close()
-        self.__fid = open(full_path, 'wb+')
-        # self.__fid.seek(0, 2)  # seek to end of file
+        self.__fid = open(full_path, 'wt')
+        stamp_str = self.__timestamp.strftime('%Y-%m-%d_%H:%M')
+
+        print(f'Data Format: {DATA_VERSION}', file=self.__fid)
+        print(f'Sensor: {self.__name}', file=self.__fid)
+        print(f'Unit: {self.__unit_str}', file=self.__fid)
+        print(f'Data Format: {str(np.dtype(self.__hw.datatype))}', file=self.__fid)
+        print(f'Start of Acquisition: {stamp_str}', file=self.__fid)
+        self.__end_of_header = self.__fid.tell()
+
+        self.__fid.flush()
         self.__fid_read = open(full_path, 'rb')
-        # self.__fid_read.seek(self.__end_of_header)
+        self.__fid_read.seek(self.__end_of_header)
 
     def get_data(self, last_ms, samples_needed):
         total_samples = int(last_ms / self.__hw.period_sampling_ms)
         step = int(total_samples/samples_needed)
         if step < 1:
             step = 1
-        # print(f'start time is {self.__timestamp}')
-        # print(f'seconds from start is {delta}')
-        # print(f'total samples is {total_samples}')
-        # print(f'step is {int(total_samples/samples_needed)}')
         bytes_to_read = total_samples * np.dtype(self.__hw.datatype).itemsize
-
         try:
-            self.__fid_read.seek(-bytes_to_read, 2)
+            if self.__fid_read.tell() <= bytes_to_read:
+                self.__fid_read.seek(self.__end_of_header)
+            else:
+                self.__fid_read.seek(-bytes_to_read, 2)
         except OSError:
             # probably occurred by reading past beginning of file
             print("Error seeking file")
-            self.__fid_read.seek(0)
+            self.__fid_read.seek(self.__end_of_header)
         data = np.fromfile(self.__fid_read, dtype=self.__hw.datatype, count=total_samples)
 
         return data
