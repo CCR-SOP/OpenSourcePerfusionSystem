7ad3bd4a77f39b9a58f66dcf0540ac4624ac5b69
John W Kakareka <kakareka@nih.gov>
2020-09-15 10:34:32 -0400

    add basic test program for testing SensorStream class

---
+++ Code/PerfusionControl/pytests/test_stream.py
@@ -0,0 +1,14 @@
+from time import sleep
+from pathlib import Path
+
+from pyPerfusion.HWAcq import HWAcq
+from pyPerfusion.SensorStream import SensorStream
+
+
+acq = HWAcq(10)
+sensor = SensorStream('test', 'ml/min', acq)
+
+sensor.open(Path('./'), Path('2020-09-14'))
+sensor.start()
+sleep(10)
+sensor.stop()
