8eaa028d66bf2387afec462f63b9c52b4ec02c41
John W Kakareka <kakareka@nih.gov>
2020-09-15 13:35:56 -0400

    update to use SensorStream Class use set_data functions for better plotting

--- Code/PerfusionControl/pyPerfusion/panel_plotting.py
+++ Code/PerfusionControl/pyPerfusion/panel_plotting.py
@@ -12,23 +12,13 @@ import matplotlib as mpl
 from matplotlib.backends.backend_wxagg import FigureCanvasWxAgg
 # from matplotlib.backends.backend_wx import NavigationToolbar2Wx
 
-
-class DummyPerfusionSensors:
-    time = {}
-    data = {}
-
-    time['HA Flow'] = np.arange(0, 10, 0.001)
-    data['HA Flow'] = np.sin(2 * np.pi * time['HA Flow'])
-
-    time['PV Flow'] = np.arange(0, 10, 0.001)
-    data['PV Flow'] = np.sin(0.1 * 2 * np.pi * time['PV Flow'])
+from pyPerfusion.SensorStream import SensorStream
 
 
 class PanelPlotting(wx.Panel):
-    def __init__(self, parent, perfusion_sensors):
-        self.parent = parent
-        self._sensors = perfusion_sensors
-        self.sensor_name = ''
+    def __init__(self, parent):
+        self.__parent = parent
+        self.__sensors = []
         wx.Panel.__init__(self, parent, -1)
 
         self.fig = mpl.figure.Figure()
@@ -41,7 +31,7 @@ class PanelPlotting(wx.Panel):
 
         self.sizer = wx.BoxSizer(wx.VERTICAL)
 
-        parameters = list(self._sensors.time.keys())
+        parameters = ['last hour', 'last day', 'study start']
         self.choice_plot_parameter = wx.Choice(self, choices=parameters)
         self.choice_plot_parameter.SetSelection(1)
         self.sensor_name = parameters[1]
@@ -52,10 +42,11 @@ class PanelPlotting(wx.Panel):
         self.__do_layout()
         self.__set_bindings()
         self.axes = self.fig.add_subplot(111)
+        self.__line = None
 
         self.timer_plot = wx.Timer(self)
         self.Bind(wx.EVT_TIMER, self.OnTimer)
-        self.timer_plot.Start(100, wx.TIMER_CONTINUOUS)
+        self.timer_plot.Start(1000, wx.TIMER_CONTINUOUS)
 
     def __do_layout(self):
 
@@ -77,21 +68,33 @@ class PanelPlotting(wx.Panel):
             self.sensor_name = new_name
             self.axes.clear()
 
-    def plot(self, sensor_name):
-        self.axes.plot(self._sensors.time[sensor_name], self._sensors.data[sensor_name], color='green')
-        self.canvas.draw()
+    def plot(self):
+        for sensor in self.__sensors:
+            data = sensor.get_data(1000, 100)
+            if data is not None:
+                data_time = np.linspace(0, len(data), num=len(data))
+                if self.__line is None:
+                    self.__line, = self.axes.plot(data_time, data)
+                else:
+                    self.__line.set_data(data_time, data)
+                    self.axes.set_ylim(50, 100)
+                    self.axes.set_xlim(data_time[0], data_time[-1])
+                self.canvas.draw()
 
     def OnTimer(self, event):
         if event.GetId() == self.timer_plot.GetId():
-            self.plot(self.sensor_name)
+            self.plot()
 
+    def add_sensor(self, sensor):
+        assert isinstance(sensor, SensorStream)
+        self.__sensors.append(sensor)
 
 
 class TestFrame(wx.Frame):
     def __init__(self, *args, **kwds):
         kwds["style"] = kwds.get("style", 0) | wx.DEFAULT_FRAME_STYLE
         wx.Frame.__init__(self, *args, **kwds)
-        self.panel = PanelPlotting(self, DummyPerfusionSensors())
+        self.panel = PanelPlotting(self)
 
 
 class MyTestApp(wx.App):
