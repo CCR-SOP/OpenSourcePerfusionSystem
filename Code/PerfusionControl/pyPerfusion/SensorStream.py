f65bfb8025b4a0950a7877da0fe3948b9ed6e4f0
John W Kakareka <kakareka@nih.gov>
2020-09-16 16:25:12 -0400

    change some private var to protected to they can be accessed by subclass open file as binary and update text-based header printing accordingly

--- Code/PerfusionControl/pyPerfusion/SensorStream.py
+++ Code/PerfusionControl/pyPerfusion/SensorStream.py
@@ -12,32 +12,32 @@ DATA_VERSION = 1
 class SensorStream(Thread):
     def __init__(self, name, unit_str, hw):
         Thread.__init__(self)
-        self.__unit_str = unit_str
-        self.__hw = hw
+        self._unit_str = unit_str
+        self._hw = hw
         self.__evt_halt = Event()
         self._fid = None
-        self.__fid_read = None
+        self._fid_read = None
         self.data = None
-        self.__name = name
+        self._name = name
         self._project_path = pathlib.Path.cwd()
-        self._filename = pathlib.Path(f'{self.__name}.dat')
+        self._filename = pathlib.Path(f'{self._name}.dat')
         self._study_path = None
-        self.__timestamp = None
+        self._timestamp = None
         self.__end_of_header = 0
-        self.__last_idx = 0
-        self.data = np.array(self.__hw.buf_len, dtype=self.__hw.data_type)
+        self._last_idx = 0
+        self.data = np.array(self._hw.buf_len, dtype=self._hw.data_type)
 
     @property
     def buf_len(self):
-        return self.__hw.buf_len
+        return self._hw.buf_len
 
     def run(self):
-        while not self.__evt_halt.wait(self.__hw.period_sampling_ms / 1000.0):
-            data_buf, t = self.__hw.get_data()
+        while not self.__evt_halt.wait(self._hw.period_sampling_ms / 1000.0):
+            data_buf, t = self._hw.get_data()
             if data_buf is not None and self._fid is not None:
                 buf_len = len(data_buf)
                 self._write_to_file(data_buf, t)
-                self.__last_idx += buf_len
+                self._last_idx += buf_len
                 self._fid.flush()
 
     def _write_to_file(self, data_buf, t):
@@ -45,7 +45,7 @@ class SensorStream(Thread):
 
     def start(self):
         super().start()
-        self.__hw.start()
+        self._hw.start()
 
     def open(self, project_path, study_path):
         if not isinstance(project_path, pathlib.Path):
@@ -57,22 +57,26 @@ class SensorStream(Thread):
         self._project_path.mkdir(parents=True, exist_ok=True)
         tmp_path = self._project_path / self._study_path
         tmp_path.mkdir(parents=True, exist_ok=True)
-        self.__timestamp = datetime.datetime.now()
+        self._timestamp = datetime.datetime.now()
         if self._fid:
             self._fid.close()
             self._fid = None
         full_path = self._project_path / self._study_path / self._filename
-        self._fid = open(full_path, 'wt')
+        self._fid = open(full_path, 'wb')
 
         self.print_header()
-
+        self._fid.flush()
+        # self._fid.close()
+        # reopen as binary for data
+        # self._fid.open(full_path, 'wb')
+        # self._fid.seek(0, SEEK_END)
         self.__end_of_header = self._fid.tell()
         self._fid.flush()
-        self.__fid_read = open(full_path, 'rb')
-        self.__fid_read.seek(self.__end_of_header)
+        self._fid_read = open(full_path, 'rb')
+        self._fid_read.seek(self.__end_of_header)
 
     def stop(self):
-        self.__hw.halt()
+        self._hw.halt()
         self.__evt_halt.set()
         # JWK, probably need a join here to ensure data collection stops before file closed
         # self.data.close()
@@ -80,38 +84,41 @@ class SensorStream(Thread):
         self._fid = None
 
     def print_header(self):
-        stamp_str = self.__timestamp.strftime('%Y-%m-%d_%H:%M')
+        stamp_str = self._timestamp.strftime('%Y-%m-%d_%H:%M')
         header = [f'File Format: {DATA_VERSION}',
-                  f'Sensor: {self.__name}',
-                  f'Unit: {self.__unit_str}',
-                  f'Data Format: {str(np.dtype(self.__hw.data_type))}',
-                  f'Sampling Period (ms): {self.__hw.period_sampling_ms}',
+                  f'Sensor: {self._name}',
+                  f'Unit: {self._unit_str}',
+                  f'Data Format: {str(np.dtype(self._hw.data_type))}',
+                  f'Sampling Period (ms): {self._hw.period_sampling_ms}',
                   f'Start of Acquisition: {stamp_str}'
                   ]
         end_of_line = '\n'
-        self._fid.writelines(f'{end_of_line.join(header)}{end_of_line}')
+        hdr_str = f'{end_of_line.join(header)}{end_of_line}'
+        self._fid.write(hdr_str.encode())
 
     def get_data(self, last_ms, samples_needed):
-
         if last_ms == 0:
-            self.__fid_read.seek(self.__end_of_header)
+            self._fid_read.seek(self.__end_of_header)
             total_samples = -1
         else:
-            total_samples = int(last_ms / self.__hw.period_sampling_ms)
-            bytes_to_read = total_samples * np.dtype(self.__hw.data_type).itemsize
+            total_samples = int(last_ms / self._hw.period_sampling_ms)
+            bytes_to_read = total_samples * np.dtype(self._hw.data_type).itemsize
             try:
-                if self.__fid_read.tell() <= bytes_to_read:
-                    self.__fid_read.seek(self.__end_of_header)
+                if self._fid_read.tell() <= bytes_to_read:
+                    self._fid_read.seek(self.__end_of_header)
                 else:
-                    self.__fid_read.seek(-bytes_to_read, 2)
+                    self._fid_read.seek(-bytes_to_read, 2)
             except OSError:
                 # probably occurred by reading past beginning of file
                 print("Error seeking file")
-                self.__fid_read.seek(self.__end_of_header)
-        data = np.fromfile(self.__fid_read, dtype=self.__hw.data_type, count=total_samples)
+                self._fid_read.seek(self.__end_of_header)
+            except AttributeError:
+                # read fid not valid
+                print("ERROR: Read FID is not valid")
+        data = np.fromfile(self._fid_read, dtype=self._hw.data_type, count=total_samples)
         idx = np.linspace(0, len(data) - 1, samples_needed, dtype='int')
-        time_start = (self.__last_idx - len(data)) * self.__hw.period_sampling_ms/1000.0 + self.__hw.start_time
-        data_time = (idx * self.__hw.period_sampling_ms/1000.0) + time_start
+        time_start = (self._last_idx - len(data)) * self._hw.period_sampling_ms / 1000.0 + self._hw.start_time
+        data_time = (idx * self._hw.period_sampling_ms / 1000.0) + time_start
         try:
             data = data[idx]
         except IndexError:
