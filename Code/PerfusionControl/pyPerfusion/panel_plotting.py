8f6f6d68c414f5c0ee5a819eeae6d05368cd58e6
John W Kakareka <kakareka@nih.gov>
2020-09-28 16:04:29 -0400

    remove long-term plot from panel_plotting to simplify class. Long-term plots will be added as a normal plot Remove choice box in anticpation of adding it back as part of a subclass

--- Code/PerfusionControl/pyPerfusion/panel_plotting.py
+++ Code/PerfusionControl/pyPerfusion/panel_plotting.py
@@ -19,59 +19,35 @@ from pyPerfusion.SensorStream import SensorStream
 from pyPerfusion.SensorPoint import SensorPoint
 
 
-class PlotFrame(Enum):
-    FROM_START = 0
-    LAST_SECOND = 1_000
-    LAST_5_SECONDS = 5_000
-    LAST_MINUTE = 60_000
-
-
 class PanelPlotting(wx.Panel):
     def __init__(self, parent):
         self.__parent = parent
         self.__sensors = []
         wx.Panel.__init__(self, parent, -1)
-        self.__plot_frame = PlotFrame.LAST_5_SECONDS
         self.__plot_len = 200
         self._valid_range = None
-
+        self.__plot_frame_ms = 5_000
 
         self.fig = mpl.figure.Figure()
         self.canvas = FigureCanvasWxAgg(self, wx.ID_ANY, self.fig)
         self.canvas.SetMinSize(wx.Size(1, 1))
         self.fig.tight_layout()
 
-        # self.toolbar = NavigationToolbar2Wx(self.canvas)
-        # self.toolbar.Realize()
-
-        self.sizer = wx.BoxSizer(wx.VERTICAL)
-
-        parameters = [item.name for item in PlotFrame]
-        self.choice_plot_parameter = wx.Choice(self, choices=parameters)
-        self.choice_plot_parameter.SetStringSelection(self.__plot_frame.name)
-        font = self.choice_plot_parameter.GetFont()
-        font.SetPointSize(12)
-        self.choice_plot_parameter.SetFont(font)
+        self.sizer = wx.BoxSizer(wx.HORIZONTAL)
 
         self.__do_layout()
         self.__set_bindings()
-        self.axes = self.fig.add_subplot(6, 1, (1, 4))
-        self.axes_lt = self.fig.add_subplot(6, 1, 6)
+        self.axes = self.fig.add_subplot(111)
         self.__line = {}
-        self.__line_lt = {}
         self.__line_range = {}
-        self.__line_range_lt = {}
         self._shaded = {}
         self.__line_invalid = {}
-        self.__line_invalid_lt = {}
 
         self.timer_plot = wx.Timer(self)
         self.Bind(wx.EVT_TIMER, self.OnTimer)
         self.timer_plot.Start(200, wx.TIMER_CONTINUOUS)
 
     def __do_layout(self):
-        self.sizer.Add(self.choice_plot_parameter, wx.SizerFlags().CenterHorizontal())
-        # self.sizer.Add(self.toolbar, 0, wx.LEFT | wx.EXPAND)
         self.sizer.Add(self.canvas, 1, wx.ALL | wx.EXPAND, border=1)
 
         self.SetSizer(self.sizer)
@@ -79,52 +55,28 @@ class PanelPlotting(wx.Panel):
         self.Layout()
 
     def __set_bindings(self):
-        self.choice_plot_parameter.Bind(wx.EVT_CHOICE, self.OnPlotChoiceChange)
-
-    def OnPlotChoiceChange(self, event):
-        self.__plot_frame = PlotFrame[self.choice_plot_parameter.GetStringSelection()]
+        pass
 
     def plot(self):
         for sensor in self.__sensors:
-            self._plot(self.__line[sensor.name], sensor, self.__plot_frame)
-            self._plot(self.__line_lt[sensor.name], sensor, PlotFrame.FROM_START)
+            self._plot(self.__line[sensor.name], sensor)
 
         self.axes.relim()
         self.axes.autoscale_view()
-        self.axes_lt.relim()
-        self.axes_lt.autoscale_view()
         self.canvas.draw()
 
-    def _plot(self, line, sensor, frame):
-        data_time, data = sensor.get_data(frame.value, self.__plot_len)
+    def _plot(self, line, sensor):
+        data_time, data = sensor.get_data(self.__plot_frame_ms, self.__plot_len)
         if data is not None and len(data) > 0:
             if type(sensor) is SensorStream:
-                self.plot_stream(line, data_time, data)
-                if frame == PlotFrame.FROM_START:
-                    try:
-                        self.axes.collections.remove(self.__line_invalid_lt[sensor.name])
-                    except ValueError:
-                        pass
-                    self.__line_invalid_lt[sensor.name] = self.axes_lt.fill_between(data_time, data, sensor.valid_range[0],
-                                                                              where=data < sensor.valid_range[0],
-                                                                              color='r')
-                else:
-                    try:
-                        self.axes_lt.collections.remove(self.__line_invalid[sensor.name])
-                    except ValueError:
-                        pass
-                    self.__line_invalid[sensor.name] = self.axes.fill_between(data_time, data, sensor.valid_range[0], where=data < sensor.valid_range[0], color='r')
+                line.set_data(data_time, data)
+                try:
+                    self.axes.collections.remove(self.__line_invalid[sensor.name])
+                except ValueError:
+                    pass
+                self.__line_invalid[sensor.name] = self.axes.fill_between(data_time, data, sensor.valid_range[0], where=data < sensor.valid_range[0], color='r')
             elif type(sensor) is SensorPoint:
-                self.plot_event(sensor, data_time, data)
-
-    def plot_stream(self, line, data_time, data):
-        if line is not None:
-            line.set_data(data_time, data)
-
-    def plot_event(self, sensor, data_time, data):
-        # del self.__line[sensor.name]
-        self.__line[sensor.name] = self.axes.vlines(data_time, ymin=0, ymax=100, color='red')
-        self.__line_lt[sensor.name] = self.axes_lt.vlines(data_time, ymin=0, ymax=100, color='red', alpha=0.2)
+                self.__line[sensor.name] = self.axes.vlines(data_time, ymin=0, ymax=100, color='red')
 
     def OnTimer(self, event):
         if event.GetId() == self.timer_plot.GetId():
@@ -135,21 +87,18 @@ class PanelPlotting(wx.Panel):
         self.__sensors.append(sensor)
         if type(sensor) is SensorStream:
             self.__line[sensor.name], = self.axes.plot([0] * self.__plot_len)
-            self.__line_lt[sensor.name], = self.axes_lt.plot([0] * self.__plot_len)
             self.__line_invalid[sensor.name] = self.axes.fill_between([0], [0], [0])
-            self.__line_invalid_lt[sensor.name] = self.axes.fill_between([0], [0], [0])
             if sensor.valid_range is not None:
                 rng = sensor.valid_range
                 self._shaded['normal'] = self.axes.axhspan(rng[0], rng[1], color='g', alpha=0.2)
-                self._shaded['lt'] = self.axes_lt.axhspan(rng[0], rng[1], color='g', alpha=0.2)
                 self._valid_range = rng
-            self.axes_lt.set_yticklabels([])
-            self.axes_lt.set_xticklabels([])
-            self.axes.set_title(sensor.name)
-            self.axes.set_ylabel(sensor.unit_str)
+            self._configure_plot(sensor)
         elif type(sensor) is SensorPoint:
             self.__line[sensor.name] = self.axes.vlines(0, ymin=0, ymax=100, color='red')
-            self.__line_lt[sensor.name] = self.axes.vlines(0, ymin=0, ymax=100, color='red', alpha=0.2)
+
+    def _configure_plot(self, sensor):
+        self.axes.set_title(sensor.name)
+        self.axes.set_ylabel(sensor.unit_str)
 
 
 class TestFrame(wx.Frame):
