4fcfe7df4262597835e4ae3eeba9dfa085e62f30
John W Kakareka <kakareka@nih.gov>
2020-09-28 12:59:19 -0400

    add new demo app to show how "Monitor 1" would be displayed

---
+++ Code/PerfusionControl/demo/demo_monitor1.py
@@ -0,0 +1,67 @@
+# -*- coding: utf-8 -*-
+"""
+
+@author: John Kakareka
+
+Demonstration of plots screen with full array of different sensors
+"""
+import wx
+import time
+from pathlib import Path
+
+from pyPerfusion.panel_plotting import PanelPlotting
+from pyPerfusion.panel_readout import PanelReadout
+from pyPerfusion.HWAcq import HWAcq
+from pyPerfusion.SensorStream import SensorStream
+
+sensors = [
+          SensorStream('HA Flow', 'ml/min', HWAcq(10, demo_amp=80, demo_offset=0)),
+          SensorStream('PV Flow', 'ml/min', HWAcq(10, demo_amp=40, demo_offset=20)),
+          SensorStream('HA Pressure', 'mmHg', HWAcq(10, demo_amp=100, demo_offset=0)),
+          SensorStream('PV Pressure', 'mmHg', HWAcq(10, demo_amp=10, demo_offset=0))
+         ]
+
+
+class TestFrame(wx.Frame):
+    def __init__(self, *args, **kwds):
+        kwds["style"] = kwds.get("style", 0) | wx.DEFAULT_FRAME_STYLE
+        wx.Frame.__init__(self, *args, **kwds)
+        sizer_main = wx.BoxSizer(wx.HORIZONTAL)
+
+        sizer_plots = wx.GridSizer(cols=2)
+        for sensor in sensors:
+            panel = PanelPlotting(self)
+            panel.add_sensor(sensor)
+
+            sizer_plots.Add(panel, 1, wx.ALL | wx.EXPAND, border=1)
+
+        sizer_readout = wx.GridSizer(cols=2)
+        for sensor in sensors:
+            panel = PanelReadout(self, sensor)
+            sizer_readout.Add(panel, 1, wx.ALL | wx.EXPAND, border=1)
+
+        for sensor in sensors:
+            sensor.open(Path('./__data__'), Path('yyyy-mm-dd'))
+            sensor.start()
+
+
+        sizer_main.Add(sizer_plots)
+        sizer_main.Add(sizer_readout)
+        self.SetSizer(sizer_main)
+        self.Fit()
+        self.Layout()
+
+
+class MyTestApp(wx.App):
+    def OnInit(self):
+        frame = TestFrame(None, wx.ID_ANY, "")
+        self.SetTopWindow(frame)
+        frame.Show()
+        return True
+
+
+app = MyTestApp(0)
+app.MainLoop()
+time.sleep(10)
+sensor.stop()
+
