a3a468b6ed463dcea81a03de169dce8397a83db7
John W Kakareka <kakareka@nih.gov>
2020-10-02 15:16:10 -0400

    Update AOSine to generate sine wave data directly as NI USB devices do not support the FuncGen functionality Generate a single cyle of the desired sine wave and repeat until stopped

--- Code/PerfusionControl/pyHardware/pyAOSine.py
+++ Code/PerfusionControl/pyHardware/pyAOSine.py
@@ -7,28 +7,45 @@ and under the public domain.
 Author: John Kakareka
 """
 import threading
+from pathlib import Path
+import numpy as np
 
 
-class AOSine:
+class AOSine(threading.Thread):
     def __init__(self, line, period_ms, volts_p2p, volts_offset, Hz, bits=12):
+        threading.Thread.__init__(self)
         self._line = line
         self._period_ms = period_ms
         self._volts_p2p = volts_p2p
         self._volts_offset = volts_offset
         self._Hz = Hz
         self._bits = bits
+        self._fid = None
 
-        self._volts_per_bit = self._calc_volts_per_bit()
+        self._data_type = np.float64
+        self._buffer = np.array([0] * 10, dtype=self._data_type)
 
-    def _calc_volts_per_bit(self):
-        vpb = self._volts_p2p / (2 ** self._bits)
-        return vpb
 
-    def _adc_to_volts(self, adc):
-        return adc * self._volts_per_bit + self._volts_offset
+        self._event_halt = threading.Event()
+        self._lock_buf = threading.Lock()
 
-    def _volts_to_adc(self, volts):
-        return int((volts - self._volt_range[0]) / self._volts_per_bit)
+        self._gen_cycle()
+
+    def _output_samples(self):
+        self._buffer.tofile(self._fid)
+
+    def run(self):
+        while not self._event_halt.wait(1.0 / self._Hz):
+            with self._lock_buf:
+                self._output_samples()
+
+    def halt(self):
+        self._event_halt.set()
+        if self._fid:
+            self._fid.close()
+
+    def open(self):
+        self._fid = open(Path('__data__') / 'sine.dat', 'w+')
 
     def set_sine(self, volts_p2p, volts_offset, Hz):
         self._volts_p2p = volts_p2p
@@ -38,3 +55,9 @@ class AOSine:
 
     def _set_sine(self):
         print(f'Creating sine {self._volts_p2p}*sine(2*pi*{self._Hz}) + {self._volts_offset}')
+        self._gen_cycle()
+
+    def _gen_cycle(self):
+        t = np.arange(0, 1 / self._Hz, step=self._period_ms / 1000.0)
+        with self._lock_buf:
+            self._buffer = self._volts_p2p / 2.0 * np.sin(2 * np.pi * self._Hz * t, dtype=self._data_type) + self._volts_offset
--- Code/PerfusionControl/pyHardware/pyAOSine_NIDAQ.py
+++ Code/PerfusionControl/pyHardware/pyAOSine_NIDAQ.py
@@ -9,6 +9,7 @@ Author: John Kakareka
 
 import time
 import threading
+import ctypes
 
 import numpy as np
 import PyDAQmx
@@ -29,13 +30,23 @@ class NIDAQ_AOSine(pyAOSine.AOSine):
     def _devname(self):
         return f"/{self.__dev}/ao{self._line}"
 
+    def _output_samples(self):
+        # super()._output_samples()
+        written = ctypes.c_int32()
+        self.__task.WriteAnalogF64(len(self._buffer), True, 1.0 / self._Hz, DAQmx_Val_GroupByChannel, self._buffer, PyDAQmx.byref(written), None)
+
     def open(self):
+        # super().open()
         try:
             if self.__task:
                 self.close()
 
             self.__task = Task()
-            self.__task.CreateAOFuncGenChan(self._devname, None, DAQmx_Val_Sine, self._Hz, self._volts_p2p, self._volts_offset)
+            # NI USB-6009 does not support FuncGen Channels
+            # self.__task.CreateAOFuncGenChan(self._devname, None, DAQmx_Val_Sine, self._Hz, self._volts_p2p, self._volts_offset)
+            volt_min = self._volts_offset - 0.5 * self._volts_p2p
+            volt_max = self._volts_offset + 0.5 * self._volts_p2p
+            self.__task.CreateAOVoltageChan(self._devname, None, 0, 5, DAQmx_Val_Volts, None)
             self.__task.StartTask()
         except PyDAQmx.DAQError as e:
             print("Could not create AO Func Channel for {}".format(self._devname))
