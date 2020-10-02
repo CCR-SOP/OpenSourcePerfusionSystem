3c0c95f2f9acc548c77ade6dfdf31302c628573d
John W Kakareka <kakareka@nih.gov>
2020-10-02 12:30:15 -0400

    add concrete class for setting the voltage on an NIDAQ analog output

---
+++ Code/PerfusionControl/pyHardware/pyAO_NIDAQ.py
@@ -0,0 +1,52 @@
+# -*- coding: utf-8 -*-
+"""Provides concrete class for controlling AO through NIDAQmx
+
+This work was created by an employee of the US Federal Gov
+and under the public domain.
+
+Author: John Kakareka
+"""
+
+import time
+import threading
+
+import numpy as np
+import PyDAQmx
+from PyDAQmx import Task
+from PyDAQmx.DAQmxConstants import *
+
+import pyHardware.pyAO as pyAO
+
+
+class NIDAQ_AO(pyAO.AO):
+    def __init__(self, line, period_ms, volt_range=[-10, 10], bits=12, dev=None):
+        super().__init__(line, period_ms, volt_range, bits)
+        self.__dev = dev
+        self.__timeout = 1.0
+        self.__task = None
+
+    @property
+    def _devname(self):
+        return f"/{self.__dev}/ao{self._line}"
+
+    def open(self):
+        try:
+            if self.__task:
+                self.close()
+
+            self.__task = Task()
+            self.__task.CreateAOVoltageChan(self._devname, None, self._volt_range[0], self._volt_range[1], DAQmx_Val_Volts, None)
+            self.__task.StartTask()
+        except PyDAQmx.DAQError as e:
+            print("Could not create AO Channel for {}".format(self._devname))
+            print(f"{e}")
+            self.__task = None
+
+    def close(self):
+        if self.__task:
+            self.__task.StopTask()
+            self.__task = None
+
+    def set_voltage(self, volts):
+        self.__task.WriteAnalogScalarF64(True, self.__timeout, volts, None)
+        super().set_voltage(volts)
