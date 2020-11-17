f0cf8afdf2395a2b2fdcf54f1ec7162b1cf5886f
John W Kakareka <kakareka@nih.gov>
2020-11-17 10:52:59 -0500

    change multiple global variables to a single global dictionary Add some helper functions to update sections

--- Code/PerfusionControl/pyPerfusion/PerfusionConfig.py
+++ Code/PerfusionControl/pyPerfusion/PerfusionConfig.py
@@ -8,21 +8,45 @@ Author: John Kakareka
 """
 import os
 from pathlib import Path
+from configparser import ConfigParser
 
-LP_PATH_BASE = Path(os.path.expanduser('~')) / 'Documents/LiverPerfusion'
-LP_PATH_CONFIG = LP_PATH_BASE / 'config'
-LP_PATH_DATA = LP_PATH_BASE / 'data'
-LP_PATH_TMP = LP_PATH_BASE / 'tmp'
+
+LP_PATH = {}
+LP_PATH['base'] = Path(os.path.expanduser('~')) / 'Documents/LiverPerfusion'
+LP_PATH['config'] = LP_PATH['base'] / 'config'
+LP_PATH['data'] = LP_PATH['base'] / 'data'
+LP_PATH['tmp'] = LP_PATH['base'] / 'tmp'
+LP_FILE = {}
+LP_FILE['hwcfg'] = LP_PATH['config'] / 'hardware.ini'
 
 
 def set_base(basepath='~/Documents'):
-    global LP_PATH_BASE, LP_PATH_CONFIG, LP_PATH_DATA, LP_PATH_TMP
+    global LP_PATH
     base = Path(os.path.expanduser(basepath))
-    LP_PATH_BASE = base / 'LiverPerfusion'
-    LP_PATH_CONFIG = LP_PATH_BASE / 'config'
-    LP_PATH_DATA = LP_PATH_BASE / 'data'
-    LP_PATH_TMP = LP_PATH_BASE / 'tmp'
-    LP_PATH_BASE.mkdir(parents=True, exist_ok=True)
-    LP_PATH_CONFIG.mkdir(parents=True, exist_ok=True)
-    LP_PATH_DATA.mkdir(parents=True, exist_ok=True)
-    LP_PATH_TMP.mkdir(parents=True, exist_ok=True)
+
+    LP_PATH['base'] = base / 'LiverPerfusion'
+    LP_PATH['config'] = LP_PATH['base'] / 'config'
+    LP_PATH['data'] = LP_PATH['base'] / 'data'
+    LP_PATH['tmp'] = LP_PATH['base'] / 'tmp'
+
+    LP_FILE['hwcfg'] = LP_PATH['config'] / 'hardware.ini'
+
+    for key in LP_PATH.keys():
+        LP_PATH[key].mkdir(parents=True, exist_ok=True)
+
+
+def update_hwcfg_section(name, updated_info):
+    config = ConfigParser()
+    config.read(LP_FILE['hwcfg'])
+    if not config.has_section(name):
+        config.add_section(name)
+    config[name] = updated_info
+    with open(LP_FILE['hwcfg'], 'w') as file:
+        config.write(file)
+
+
+def get_hwcfg_section(name):
+    config = ConfigParser()
+    config.read(LP_FILE['hwcfg'])
+    section = config[name]
+    return section
