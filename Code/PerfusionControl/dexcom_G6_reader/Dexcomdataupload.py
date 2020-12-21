fbfa71ce3923612baabe8df63240b4348cb6b85c
Allen Luna <aluna2313@gmail.com>
2020-12-21 16:30:52 -0500

    Updated loading of modules due to previous change in subfolder structure

--- Code/PerfusionControl/dexcom_G6_reader/Dexcomdataupload.py
+++ Code/PerfusionControl/dexcom_G6_reader/Dexcomdataupload.py
@@ -1,10 +1,10 @@
 from __future__ import print_function  # python 2 and 3 compatible
 
-import dexcom_G6_reader.constants as constants
-import dexcom_G6_reader.util as util
-import dexcom_G6_reader.crc16 as crc16
-import dexcom_G6_reader.database_records as database_records
-import dexcom_G6_reader.packetwriter as packetwriter
+import constants
+import util
+import crc16
+import database_records
+import packetwriter
 import datetime
 import serial
 import sys
