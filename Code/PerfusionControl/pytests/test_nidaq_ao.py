94f89aceae631ac0cdc0b076c663c6e615062e4e
John W Kakareka <kakareka@nih.gov>
2021-01-25 11:35:23 -0500

    add a loop to send more than one buffer. First buffer does not seem to get sent correctly, but subsequent buffer do add non-OO code to test if OO code was buggy (commented out, OO and non-OO code behaved the same)

--- Code/PerfusionControl/pytests/test_nidaq_ao.py
+++ Code/PerfusionControl/pytests/test_nidaq_ao.py
@@ -1,6 +1,6 @@
+import time
+
 import PyDAQmx
-from PyDAQmx import Task
-from PyDAQmx.DAQmxConstants import *
 import numpy as np
 import ctypes
 
@@ -25,12 +25,27 @@ samples_per_channel_written = ctypes.c_int(0)  # Place holder for a single numbe
 # print(f'samples written is {samples_per_channel_written}')
 # task.StartTask()
 
-t = np.arange(0, 3.0, step=0.001)
-buffer = 1.0 * np.sin(2 * np.pi * 1 * t, dtype=np.float64) + 1.75
-task = Task()
-task.CreateAOVoltageChan("Dev2/ao0", None, -10, 10, DAQmx_Val_Volts, None)
-task.CfgSampClkTiming(None, 100, PyDAQmx.DAQmx_Val_Rising,
-                      PyDAQmx.DAQmx_Val_FiniteSamps, len(buffer))
-task.WriteAnalogF64(len(buffer), 1, 5.0, PyDAQmx.DAQmx_Val_GroupByChannel,
-                    buffer, PyDAQmx.byref(samples_per_channel_written), None)
+t = np.arange(0, 1.0, step=0.01)
+buffer = 1.0 * np.sin(2 * np.pi * 5 * t, dtype=np.float64) + 1.75
+
+task = PyDAQmx.Task()
+task.CreateAOVoltageChan("Dev3/ao0", None, -10, 10, PyDAQmx.DAQmx_Val_Volts, None)
+task.CfgSampClkTiming("", 100.0, PyDAQmx.DAQmx_Val_Rising, PyDAQmx.DAQmx_Val_FiniteSamps, 10000)
+#task.WriteAnalogF64(1000, 1, 10.0, PyDAQmx.DAQmx_Val_GroupByChannel,
+#                    buffer, PyDAQmx.byref(samples_per_channel_written), None)
+#task.StartTask()
+for i in range(1, 10):
+    print(f'loop {i}')
+    task.WriteAnalogF64(100, 1, 10.0, PyDAQmx.DAQmx_Val_GroupByChannel,
+                        buffer, PyDAQmx.byref(samples_per_channel_written), None)
+    # time.sleep(0.1)
 print(f'samples written is {samples_per_channel_written}')
+
+# Try TaskHandle Method (DOES NOT WORK EITHER)
+# task_handle = PyDAQmx.TaskHandle()
+# PyDAQmx.DAQmxCreateTask("", PyDAQmx.byref(task_handle))
+# PyDAQmx.DAQmxCreateAOVoltageChan(task_handle, "Dev3/ao0", "", -10, 10, DAQmx_Val_Volts, "")
+# PyDAQmx.DAQmxCfgSampClkTiming(task_handle, "OnboardClock", 1000.0, DAQmx_Val_Rising, DAQmx_Val_ContSamps, len(buffer))
+# PyDAQmx.DAQmxWriteAnalogF64(task_handle, len(buffer), 0, 10.0, DAQmx_Val_GroupByChannel,
+#                             buffer, None, None)
+# PyDAQmx.DAQmxStartTask(task_handle)
