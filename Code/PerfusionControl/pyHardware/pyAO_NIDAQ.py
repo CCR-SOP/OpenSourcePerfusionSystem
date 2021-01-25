7d5ac0fd5b795cf85282242e34834e4514c04fb5
John W Kakareka <kakareka@nih.gov>
2021-01-25 11:37:18 -0500

    updates so sine wave (basically) works: only one call to CfgSampClkTiming, try/except around Write function, hard-coded timeout as it seems to require a much larger timeout than anticipated

--- Code/PerfusionControl/pyHardware/pyAO_NIDAQ.py
+++ Code/PerfusionControl/pyHardware/pyAO_NIDAQ.py
@@ -43,11 +43,18 @@ class NIDAQ_AO(pyAO.AO):
         if self.__task:
             if self._Hz > 0:
                 written = ctypes.c_int32(0)
-                self.__task.WriteAnalogF64(len(self._buffer) / 2.0, True, 1.0 / self._Hz * 1.1, DAQmx_Val_GroupByChannel, self._buffer, PyDAQmx.byref(written), None)
+                try:
+                    #task.WriteAnalogF64(100, 1, 10.0, PyDAQmx.DAQmx_Val_GroupByChannel,
+                    #                    buffer, PyDAQmx.byref(samples_per_channel_written), None)
+                    print(f'len of buffer is {len(self._buffer)}')
+                    self.__task.WriteAnalogF64(len(self._buffer), True, 2.0, DAQmx_Val_GroupByChannel,
+                                               self._buffer, PyDAQmx.byref(written), None)
+                except PyDAQmx.DAQmxFunctions.SamplesCanNotYetBeWrittenError as err:
+                    print('SamplesCanNotYetBeWrittenError')
                 print(f'written is {written}')
             else:
                 if self._volts_offset != self.__last_dc_val:
-                    self.__task.WriteAnalogScalarF64(True, self.__timeout, self._volts_offset, None)
+                    # self.__task.WriteAnalogScalarF64(True, self.__timeout, self._volts_offset, None)
                     self.__last_dc_val = self._volts_offset
 
     def open(self, line, period_ms, bits=12, dev=None):
@@ -63,6 +70,7 @@ class NIDAQ_AO(pyAO.AO):
             # self.__task.CreateAOFuncGenChan(self._devname, None, DAQmx_Val_Sine, self._Hz, self._volts_p2p, self._volts_offset)
 
             self.__task.CreateAOVoltageChan(self._devname, None, 0, 5, DAQmx_Val_Volts, None)
+            self.__task.CfgSampClkTiming("", 100.0, PyDAQmx.DAQmx_Val_Rising, PyDAQmx.DAQmx_Val_ContSamps, 200)
         except PyDAQmx.DAQError as e:
             print("Could not create AO Func Channel for {}".format(self._devname))
             print(f"{e}")
@@ -85,9 +93,9 @@ class NIDAQ_AO(pyAO.AO):
 
     def set_sine(self, volts_p2p, volts_offset, Hz):
         super().set_sine(volts_p2p, volts_offset, Hz)
-        self.__update_timing()
+        # self.__update_timing()
 
     def set_dc(self, volts):
         self.__last_dc_val = None
         super().set_dc(volts)
-        self.__update_timing()
+        # self.__update_timing()
