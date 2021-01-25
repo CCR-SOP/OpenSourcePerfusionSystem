4b272a5d0c52bdcc518ee807be304805a638146e
John W Kakareka <kakareka@nih.gov>
2021-01-25 12:49:37 -0500

    When updates to sine or dc, stop and clear existing task and recreate.

--- Code/PerfusionControl/pyHardware/pyAO_NIDAQ.py
+++ Code/PerfusionControl/pyHardware/pyAO_NIDAQ.py
@@ -7,6 +7,7 @@ and under the public domain.
 Author: John Kakareka
 """
 import ctypes
+import time
 
 import PyDAQmx
 from PyDAQmx import Task
@@ -45,21 +46,7 @@ class NIDAQ_AO(pyAO.AO):
     def open(self, line, period_ms, bits=12, dev=None):
         self.__dev = dev
         super().open(line, period_ms, bits)
-        try:
-            if self.__task:
-                self.close()
-
-            self.__task = Task()
-
-            # NI USB-6009 does not support FuncGen Channels
-            # self.__task.CreateAOFuncGenChan(self._devname, None, DAQmx_Val_Sine, self._Hz, self._volts_p2p, self._volts_offset)
-
-            self.__task.CreateAOVoltageChan(self._devname, None, 0, 5, DAQmx_Val_Volts, None)
-            self.__task.CfgSampClkTiming("", 100.0, PyDAQmx.DAQmx_Val_Rising, PyDAQmx.DAQmx_Val_ContSamps, 200)
-        except PyDAQmx.DAQError as e:
-            print("Could not create AO Func Channel for {}".format(self._devname))
-            print(f"{e}")
-            self.__task = None
+        self.__task = Task()
 
     def close(self):
         self.halt()
@@ -67,25 +54,22 @@ class NIDAQ_AO(pyAO.AO):
             self.__task.StopTask()
             self.__task = None
 
-    def __update_timing(self):
-        if self.__task:
-            buf_len = len(self._buffer)
-            self.__task.StopTask()
-            rate = 1.0 / (self._period_ms / 1000.0)
-            self.__task.CfgSampClkTiming(None, rate,
-                                         DAQmx_Val_Rising, DAQmx_Val_ContSamps,
-                                         buf_len)
+    def __clear_and_create_task(self):
+        self.__task.StopTask()
+        self.__task.ClearTask()
+        self.__task = Task()
+        self.__task.CreateAOVoltageChan(self._devname, None, 0, 5, DAQmx_Val_Volts, None)
 
     def set_sine(self, volts_p2p, volts_offset, Hz):
         super().set_sine(volts_p2p, volts_offset, Hz)
         written = ctypes.c_int32(0)
-        self.__task.StopTask()
-        self.__task.WriteAnalogF64(len(self._buffer), True, 2.0, DAQmx_Val_GroupByChannel,
+        self.__clear_and_create_task()
+        self.__task.CfgSampClkTiming("", 100.0, PyDAQmx.DAQmx_Val_Rising, PyDAQmx.DAQmx_Val_ContSamps, 200)
+        self.__task.WriteAnalogF64(len(self._buffer), True, self.__timeout, PyDAQmx.DAQmx_Val_GroupByChannel,
                                    self._buffer, PyDAQmx.byref(written), None)
-        # self.__update_timing()
 
     def set_dc(self, volts):
         self.__last_dc_val = None
         super().set_dc(volts)
+        self.__clear_and_create_task()
         self.__task.WriteAnalogScalarF64(True, self.__timeout, self._volts_offset, None)
-        # self.__update_timing()
