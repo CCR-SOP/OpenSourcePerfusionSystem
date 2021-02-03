f73c4cfd3863538eb1dd54efd79d7c4901ab3e8e
John Kakareka <kakareka@nih.gov>
2021-02-03 13:10:29 -0500

    update simple test_ao app to test for some recent bugs (open/close, setting voltage to current value, waiting for task to complete)

--- Code/PerfusionControl/pytests/test_ao.py
+++ Code/PerfusionControl/pytests/test_ao.py
@@ -3,10 +3,24 @@ from time import sleep
 from pyHardware.pyAO_NIDAQ import NIDAQ_AO
 
 ao = NIDAQ_AO()
+dev = 'Dev4'
+line = 0
 
-ao.open(line=0, period_ms=1, dev='Dev2')
-ao.set_sine(2, 2, 1)
+print('Opening device')
+ao.open(line=line, period_ms=1, dev=dev)
+print('Closing device')
+ao.close()
 
+print('Opening device')
+ao.open(line=line, period_ms=1, dev=dev)
+
+print('Starting AO')
 ao.start()
-sleep(5.0)
-ao.halt()
+print('Setting to 2.5V')
+ao.set_dc(2.5)
+ao.wait_for_task()
+print('Setting to 2.5V')
+ao.set_dc(2.5)
+ao.wait_for_task()
+print('Closing device')
+ao.close()
