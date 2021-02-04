fd4f6fb5525685a3f38b9e199b70042bc54ba747
John W Kakareka <kakareka@nih.gov>
2021-02-04 09:10:20 -0500

    add additiona AO test cases for multiple simultaneous AO being controlled

--- Code/PerfusionControl/pytests/test_ao.py
+++ Code/PerfusionControl/pytests/test_ao.py
@@ -2,25 +2,59 @@ from time import sleep
 
 from pyHardware.pyAO_NIDAQ import NIDAQ_AO
 
-ao = NIDAQ_AO()
+ao1 = NIDAQ_AO()
+ao2 = NIDAQ_AO()
 dev = 'Dev4'
 line = 0
 
 print('Opening device')
-ao.open(line=line, period_ms=1, dev=dev)
+ao1.open(line=line, period_ms=1, dev=dev)
 print('Closing device')
-ao.close()
+ao1.close()
 
 print('Opening device')
-ao.open(line=line, period_ms=1, dev=dev)
+ao1.open(line=line, period_ms=1, dev=dev)
 
 print('Starting AO')
-ao.start()
+ao1.start()
 print('Setting to 2.5V')
-ao.set_dc(2.5)
-ao.wait_for_task()
+ao1.set_dc(2.5)
+ao1.wait_for_task()
 print('Setting to 2.5V')
-ao.set_dc(2.5)
-ao.wait_for_task()
+ao1.set_dc(2.5)
+ao1.wait_for_task()
 print('Closing device')
-ao.close()
+ao1.close()
+
+print('Opening first device')
+ao1.open(line=line, period_ms=1, dev=dev)
+print('Opening second device')
+ao2.open(line=line+1, period_ms=1, dev=dev)
+
+print('Starting AO')
+ao1.start()
+ao2.start()
+print('Setting first line to 2.5V, second line to 1.0V')
+ao1.set_dc(2.5)
+ao1.set_dc(1.0)
+ao1.wait_for_task()
+ao2.wait_for_task()
+
+
+print('Setting first line to 2.5V, second line to sine ')
+ao1.set_dc(2.5)
+ao2.set_sine(volts_p2p=2, volts_offset=1, Hz=2.0)
+print('pausing 2.0 seconds')
+sleep(2.0)
+
+
+print('Setting first line to sine, second line to sine ')
+ao1.set_sine(volts_p2p=1, volts_offset=0.5, Hz=1.0)
+ao2.set_sine(volts_p2p=2, volts_offset=1, Hz=2.0)
+print('pausing 2.0 seconds')
+sleep(2.0)
+
+
+print('Closing devices')
+ao1.close()
+ao2.close()
