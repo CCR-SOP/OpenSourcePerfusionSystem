ecd5aab551c682bd4023a9d38efec901e2e28202
Allen Luna <aluna2313@gmail.com>
2021-02-22 17:02:49 -0500

    Finished basic code for running pressure controlled perfusions

--- Code/PerfusionControl/pyPerfusion/panel_pressure_controlled_perfusion.py
+++ Code/PerfusionControl/pyPerfusion/panel_pressure_controlled_perfusion.py
@@ -7,6 +7,7 @@ import wx
 
 from pyHardware.pyAO_NIDAQ import NIDAQ_AO
 from pyHardware.pyAI_NIDAQ import NIDAQ_AI
+from pyPerfusion.panel_AI import PanelAI
 from pyPerfusion.panel_plotting import PanelPlotting
 from pyPerfusion.SensorStream import SensorStream
 import pyPerfusion.PerfusionConfig as LP_CFG
@@ -18,31 +19,15 @@ class PanelTestPressure(wx.Panel):
         self._name = name
         self._dev = dev
         self._line = line
-        self._inc = 0.025
-
         self._ao = NIDAQ_AO()
-        self._ao.open(period_ms=100, dev=dev, line=line)
-        self._ao.set_dc(0)
+        self._inc = 0.025
 
         wx.Panel.__init__(self, parent, -1)
 
-        self.sizer = wx.BoxSizer(wx.VERTICAL)
+        static_box = wx.StaticBox(self, wx.ID_ANY, label=name)
+        self.sizer = wx.StaticBoxSizer(static_box, wx.HORIZONTAL)
 
-        self.label_cal_pt1 = wx.StaticText(self, label="Calibration Pt 1")
-        self.spin_cal_pt1 = wx.SpinCtrlDouble(self, min=0, max=1000, inc=self._inc)
-        self.label_cal_pt2 = wx.StaticText(self, label="Calibration Pt 2")
-        self.spin_cal_pt2 = wx.SpinCtrlDouble(self, min=0, max=1000, inc=self._inc)
-        self.label_cal_pt1_val = wx.StaticText(self, label='No reading')
-        self.label_cal_pt2_val = wx.StaticText(self, label='No reading')
-        self.btn_cal_pt1 = wx.Button(self, label='Read Cal Pt 1')
-        self.btn_cal_pt2 = wx.Button(self, label='Read Cal Pt 2')
-        self.btn_calibrate = wx.Button(self, label='Calibrate')
-        self.btn_reset_cal = wx.Button(self, label='Reset Cal')
-        self.btn_save_settings = wx.Button(self, label='Save Settings')
-        self.btn_load_settings = wx.Button(self, label='Load Settings')
-        self.btn_start_calibration = wx.ToggleButton(self, label='Start Calibration')
-
-        self.label_desired_output = wx.StaticText(self, label='Desired Output')
+        self.label_desired_output = wx.StaticText(self, label='Desired ' + self._name)
         self.spin_desired_output = wx.SpinCtrlDouble(self, min=0.0, max=100, initial=50, inc=self._inc)
 
         self.label_tolerance = wx.StaticText(self, label='Tolerance (mmHg)')
@@ -53,139 +38,53 @@ class PanelTestPressure(wx.Panel):
 
         self.btn_stop = wx.ToggleButton(self, label='Start')
 
-        self.panel_plot = PanelPlotting(self)
-        self.panel_plot.add_sensor(self._sensor)
-        LP_CFG.update_stream_folder()
-        self._sensor.open(LP_CFG.LP_PATH['stream'])
-
         self.__do_layout()
         self.__set_bindings()
-
-        self._sensor.start()
 
         self.timer_pressure_adjust = wx.Timer(self)
         self.Bind(wx.EVT_TIMER, self.OnTimer)
 
     def __do_layout(self):
-        flags = wx.SizerFlags().Expand()
+        flags = wx.SizerFlags().Border(wx.ALL, 5).Left().Proportion(0)
 
-        sizer = wx.BoxSizer(wx.HORIZONTAL)
-        sizer.Add(self.btn_save_settings, flags)
-        sizer.AddSpacer(5)
-        sizer.Add(self.btn_load_settings, flags)
-        self.sizer.Add(sizer)
+        self.sizer_label = wx.BoxSizer(wx.HORIZONTAL)
+        self.sizer_label.Add(self.label_desired_output, flags)
+        self.sizer_label.Add(self.spin_desired_output, flags)
 
-        sizer = wx.GridSizer(cols=4)
-        sizer.Add(self.label_cal_pt1, flags)
-        sizer.Add(self.spin_cal_pt1, flags)
-        sizer.Add(self.btn_cal_pt1, flags)
-        sizer.Add(self.label_cal_pt1_val, flags)
-        sizer.Add(self.label_cal_pt2, flags)
-        sizer.Add(self.spin_cal_pt2, flags)
-        sizer.Add(self.btn_cal_pt2, flags)
-        sizer.Add(self.label_cal_pt2_val, flags)
-        sizer.Add(self.btn_calibrate, flags)
-        sizer.Add(self.btn_reset_cal, flags)
-        sizer.Add(self.btn_start_calibration)
-        self.sizer.Add(sizer)
-        self.sizer.AddSpacer(20)
+        self.sizer_tol = wx.BoxSizer(wx.HORIZONTAL)
+        self.sizer_tol.Add(self.label_tolerance, flags)
+        self.sizer_tol.Add(self.spin_tolerance, flags)
 
-        sizer = wx.BoxSizer(wx.HORIZONTAL)
-        sizer.Add(self.label_desired_output, flags)
-        sizer.Add(self.spin_desired_output, flags)
-        sizer.AddSpacer(20)
-        sizer.Add(self.label_tolerance, flags)
-        sizer.Add(self.spin_tolerance, flags)
-        sizer.AddSpacer(20)
-        sizer.Add(self.label_increment, flags)
-        sizer.Add(self.spin_increment, flags)
-        sizer.AddSpacer(20)
+        self.sizer_increment = wx.BoxSizer(wx.HORIZONTAL)
+        self.sizer_increment.Add(self.label_increment, flags)
+        self.sizer_increment.Add(self.spin_increment, flags)
+
+        sizer = wx.GridSizer(cols=1)
+        sizer.Add(self.sizer_label)
+        sizer.Add(self.sizer_tol)
+        sizer.Add(self.sizer_increment, flags)
         sizer.Add(self.btn_stop, flags)
         self.sizer.Add(sizer)
-        self.sizer.AddSpacer(20)
 
-        self.sizer.Add(self.panel_plot, 1, wx.ALL | wx.EXPAND, border=1)
-
-        self.SetSizer(self.sizer)
+        sizer = wx.BoxSizer(wx.VERTICAL)
+        sizer.Add(self.sizer, 1, wx.EXPAND | wx.ALL, border=5)
+        self.SetSizer(sizer)
         self.Layout()
         self.Fit()
 
     def __set_bindings(self):
-        self.btn_start_calibration.Bind(wx.EVT_TOGGLEBUTTON, self.OnStartCalibration)
         self.btn_stop.Bind(wx.EVT_TOGGLEBUTTON, self.OnStartStop)
-        self.btn_save_settings.Bind(wx.EVT_BUTTON, self.OnSaveSettings)
-        self.btn_load_settings.Bind(wx.EVT_BUTTON, self.OnLoadSettings)
-        self.btn_cal_pt1.Bind(wx.EVT_BUTTON, self.OnCalPt1)
-        self.btn_cal_pt2.Bind(wx.EVT_BUTTON, self.OnCalPt2)
-        self.btn_calibrate.Bind(wx.EVT_BUTTON, self.OnCalibrate)
-        self.btn_reset_cal.Bind(wx.EVT_BUTTON, self.OnResetCalibration)
-
-    def OnStartCalibration(self, evt):
-        state = self.btn_start_calibration.GetLabel()
-        if state == 'Start Calibration':
-            self.btn_stop.Enable(False)
-            self._sensor.hw.open(dev='Dev1', line=0)  # HA Pressure Sensor
-            self._sensor.hw.start()
-            self.btn_start_calibration.SetLabel('Stop Calibration')
-        else:
-            self.btn_stop.Enable(True)
-            self._sensor.hw.stop()
-            self._sensor.hw.close()
-            self.btn_start_calibration.SetLabel('Start Calibration')
 
     def OnStartStop(self, evt):
         state = self.btn_stop.GetLabel()
         if state == 'Start':
-            self.btn_start_calibration.Enable(False)
-            self._sensor.hw.open(dev='Dev1', line=0)  # HA Pressure Sensor
-            self._sensor.hw.start()
-            self.btn_stop.SetLabel('Stop')
+            self._ao.open(period_ms=100, dev=self._dev, line=self._line)
+            self._ao.set_dc(0)
             self.timer_pressure_adjust.Start(1000, wx.TIMER_CONTINUOUS)
         else:
-            self.btn_start_calibration.Enable(True)
-            self._sensor.hw.stop()
-            self._sensor.hw.close()
-            self.btn_stop.SetLabel('Start')
             self.timer_pressure_adjust.Stop()
             self._ao.close()
             self._ao.halt()
-
-    def OnCalPt1(self, evt):
-        val = self._sensor.get_current()
-        self.label_cal_pt1_val.SetLabel(f'{val:.3f}')
-
-    def OnCalPt2(self, evt):
-        val = self._sensor.get_current()
-        self.label_cal_pt2_val.SetLabel(f'{val:.3f}')
-
-    def OnCalibrate(self, evt):
-        low_pt = self.spin_cal_pt1.GetValue()
-        low_read = float(self.label_cal_pt1_val.GetLabel())
-        high_pt = self.spin_cal_pt2.GetValue()
-        high_read = float(self.label_cal_pt2_val.GetLabel())
-        self._sensor.hw.set_2pt_cal(low_pt, low_read, high_pt, high_read)
-
-    def OnResetCalibration(self, evt):
-        self._sensor.hw.set_2pt_cal(0, 0, 1, 1)
-        self.spin_cal_pt1.SetValue(0)
-        self.spin_cal_pt2.SetValue(1)
-        self.label_cal_pt1_val.SetLabel('0')
-        self.label_cal_pt2_val.SetLabel('1')
-
-    def OnSaveSettings(self, evt):
-        section = LP_CFG.get_hwcfg_section('HA')
-        section['CalPt1_Target'] = f'{self.spin_cal_pt1.GetValue():.3f}'
-        section['CalPt1_Reading'] = self.label_cal_pt1_val.GetLabel()
-        section['CalPt2_Target'] = f'{self.spin_cal_pt2.GetValue():.3f}'
-        section['CalPt2_Reading'] = self.label_cal_pt2_val.GetLabel()
-        LP_CFG.update_hwcfg_section(self._name, section)
-
-    def OnLoadSettings(self, evt):
-        section = LP_CFG.get_hwcfg_section('HA')
-        self.spin_cal_pt1.SetValue(float(section['CalPt1_Target']))
-        self.label_cal_pt1_val.SetLabel(section['CalPt1_Reading'])
-        self.spin_cal_pt2.SetValue(float(section['CalPt2_Target']))
-        self.label_cal_pt2_val.SetLabel(section['CalPt2_Reading'])
 
     def OnTimer(self, event):
         if event.GetId() == self.timer_pressure_adjust.GetId():
@@ -206,7 +105,6 @@ class PanelTestPressure(wx.Panel):
                 new_val = self._ao._volts_offset - inc
             self._ao.set_dc(new_val)
 
-
 class TestFrame(wx.Frame):
     def __init__(self, *args, **kwds):
         kwds["style"] = kwds.get("style", 0) | wx.DEFAULT_FRAME_STYLE
@@ -219,6 +117,7 @@ class TestFrame(wx.Frame):
         }
 
         for sensor, pump in self.pressure_sensors.items():
+            sizer.Add(PanelAI(self, sensor, name=sensor.name), 1, wx.ALL | wx.EXPAND, border=1)
             sizer.Add(PanelTestPressure(self, sensor, name=sensor.name, dev=pump[0], line=pump[1]), 1, wx.ALL | wx.EXPAND, border=1)
 
         self.SetSizer(sizer)
@@ -227,7 +126,7 @@ class TestFrame(wx.Frame):
         self.Bind(wx.EVT_CLOSE, self.OnClose)
 
     def OnClose(self, evt):
-        for sensor in self.pressure_sensors:
+        for sensor in self.pressure_sensors.keys():
             sensor.stop()
         self.Destroy()
 
