c16286ee18228f4606e4cb52345a827151d06821
John W Kakareka <kakareka@nih.gov>
2021-03-25 14:57:02 -0400

    cleanup logging utils, add filename parameter for better control of log files

--- Code/PerfusionControl/pyPerfusion/utils.py
+++ Code/PerfusionControl/pyPerfusion/utils.py
@@ -8,13 +8,15 @@ This work was created by an employee of the US Federal Gov
 and under the public domain.
 """
 import logging
+import warnings
+
 from pyPerfusion.PerfusionConfig import LP_PATH
 
 
-def setup_default_logging():
+def setup_default_logging(filename=None):
     setup_stream_logger(logging.getLogger(), logging.INFO)
-    setup_file_logger(logging.getLogger(), logging.DEBUG)
-    disable_matplotlib_debug()
+    setup_file_logger(logging.getLogger(), logging.DEBUG, filename)
+    configure_matplotlib_logging()
 
 
 def setup_stream_logger(lgr, level):
@@ -37,5 +39,15 @@ def setup_file_logger(lgr, level, filename=None):
     lgr.addHandler(ch)
 
 
-def disable_matplotlib_debug():
-    logging.getLogger("matplotlib").setLevel(logging.WARNING)
+def filter_legend_messages(record):
+    print(record.module)
+    if record.module == 'matplotlib.legend':
+        return False
+    return True
+
+def configure_matplotlib_logging():
+    # matplotlib logs to the root logger and can create
+    # a lot of debug messages, this will only log warning
+    # or higher levels
+    lgr = logging.getLogger('matplotlib')
+    lgr.setLevel(logging.WARNING)
