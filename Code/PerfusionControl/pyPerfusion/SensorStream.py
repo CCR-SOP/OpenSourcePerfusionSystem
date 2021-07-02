c1c42840359e8501c36d33a905d027701feeb220
John W Kakareka <kakareka@nih.gov>
2021-07-02 13:00:22 -0400

    remove direct saving to file from SensorStream and use strategy Support multiple strategies

--- Code/PerfusionControl/pyPerfusion/SensorStream.py
+++ Code/PerfusionControl/pyPerfusion/SensorStream.py
@@ -6,6 +6,8 @@ import time
 
 import numpy as np
 
+from pyPerfusion.ProcessingStrategy import ProcessingStrategy, SaveStreamToFile
+
 
 DATA_VERSION = 1
 
@@ -20,24 +22,21 @@ class SensorStream:
         self.hw = hw
         self._ch_id = None
         self.__evt_halt = Event()
-        self._fid_write = None
         self.data = None
         self.name = name
-        self._full_path = pathlib.Path.cwd()
-        self._filename = pathlib.Path(f'{self.name}')
-        self._ext = '.dat'
         self._timestamp = None
-        self._end_of_header = 0
-        self._last_idx = 0
         self.data = np.array(self.hw.buf_len, dtype=self.hw.data_type)
+        self._strategies = []
+        self._params = {'Sensor': self.name,
+                        'Unit': self._unit_str,
+                        'Data Format': str(np.dtype(self.hw.data_type)),
+                        'Sampling Period (ms)': self.hw.period_sampling_ms,
+                        'Start of Acquisition': 0
+                        }
 
     @property
     def buf_len(self):
         return self.hw.buf_len
-
-    @property
-    def full_path(self):
-        return self._full_path / self._filename.with_suffix(self._ext)
 
     @property
     def unit_str(self):
@@ -51,6 +50,9 @@ class SensorStream:
     def ch_id(self):
         return self._ch_id
 
+    def add_strategy(self, strategy: ProcessingStrategy):
+        self._strategies.append(strategy)
+
     def run(self):
         next_t = time.time()
         offset = 0
@@ -63,29 +65,16 @@ class SensorStream:
             else:
                 offset = -delay
             data_buf, t = self.hw.get_data(self._ch_id)
-            if data_buf is not None and self._fid_write is not None:
-                self._write_raw_to_file(data_buf, t)
-                if self._process:
-                    proc_buffer = self._process.process_buffer(data_buf)
-
-
-    def _write_raw_to_file(self, data_buf, t):
-            buf_len = len(data_buf)
-            self._write_to_file(data_buf, t)
-            self._last_idx += buf_len
-            self._fid_write.flush()
-
-    def _write_to_file(self, data_buf, t):
-        data_buf.tofile(self._fid_write)
+            if data_buf is not None:
+                buf = data_buf
+                for strategy in self._strategies:
+                    buf = strategy.process_buffer(buf)
 
     def _open_read(self):
-        _fid = open(self.full_path, 'rb')
+        # Assumes first strategy is the raw data strategy
+        _fid = open(self._strategies[0].fqpn, 'rb')
         data = np.memmap(_fid, dtype=self.hw.data_type, mode='r')
         return _fid, data
-
-    def _open_write(self):
-        self._logger.info(f'opening {self.full_path}')
-        self._fid_write = open(self.full_path, 'w+b')
 
     def start(self):
         if self.__thread:
@@ -95,25 +84,11 @@ class SensorStream:
         self._ch_id = ch_id
 
     def open(self, full_path):
-        if not isinstance(full_path, pathlib.Path):
-            full_path = pathlib.Path(full_path)
-        self._full_path = full_path
-        if not self._full_path.exists():
-            self._full_path.mkdir(parents=True, exist_ok=True)
+        strategy = SaveStreamToFile(f'{self.name}_raw', None, self.hw.buf_len)
         self._timestamp = datetime.datetime.now()
-        if self._fid_write:
-            self._fid_write.close()
-            self._fid_write = None
-
-        # write file handle should be opened first as the memory mapped read handle needs
-        # a file with data in it
-        self._open_write()
-        self._write_to_file(np.array([0]), np.array([0]))
-        # reset file point to start to overwrite the dummy value with valid data when it arrives
-        self._fid_write.seek(0)
-        # self._open_read()
-
-        self.print_stream_info()
+        self._params['Start of Acquisition'] = self._timestamp.strftime('%Y-%m-%d_%H:%M')
+        strategy.open(full_path, self._params)
+        self.add_strategy(strategy)
         self.__thread = Thread(target=self.run)
         self.__thread.name = f'SensorStream ({self.name})'
 
@@ -121,30 +96,8 @@ class SensorStream:
         self.__evt_halt.set()
         if self.__thread:
             self.__thread.join(2.0)
-        if self._fid_write:
-            self._fid_write.close()
-        self._fid_write = None
-
-    def _get_stream_info(self):
-        stamp_str = self._timestamp.strftime('%Y-%m-%d_%H:%M')
-        header = [f'File Format: {DATA_VERSION}',
-                  f'Sensor: {self.name}',
-                  f'Unit: {self._unit_str}',
-                  f'Data Format: {str(np.dtype(self.hw.data_type))}',
-                  f'Sampling Period (ms): {self.hw.period_sampling_ms}',
-                  f'Start of Acquisition: {stamp_str}'
-                  ]
-        end_of_line = '\n'
-        hdr_str = f'{end_of_line.join(header)}{end_of_line}'
-        return hdr_str
-
-    def print_stream_info(self):
-        hdr_str = self._get_stream_info()
-        filename = self.full_path.with_suffix('.txt')
-        self._logger.debug(f"printing stream info to {filename}")
-        fid = open(filename, 'wt')
-        fid.write(hdr_str)
-        fid.close()
+        for strategy in self._strategies:
+            strategy.close()
 
     def get_data(self, last_ms, samples_needed):
         _fid, data = self._open_read()
