b860adfaa83d42b9572c1a34de9569e4a84f5db6
Allen Luna <aluna2313@gmail.com>
2022-03-23 15:28:23 -0400

    Updated code for streaming data from TSM

--- Code/PerfusionControl/pyPerfusion/panel_gb100_saturation_monitor.py
+++ Code/PerfusionControl/pyPerfusion/panel_gb100_saturation_monitor.py
@@ -9,7 +9,7 @@ import logging
 import pyPerfusion.utils as utils
 import os
 from pathlib import Path
-from pyPerfusion.plotting import PanelPlotting, PanelPlotLT, SensorPlot
+from pyPerfusion.plotting_TSM_GB100 import TSMPanelPlotting, TSMPanelPlotLT, TSMSensorPlot
 from pyHardware.pySaturationMonitor import TSMSerial
 from pyHardware.pyGB100 import GB100
 import pyPerfusion.PerfusionConfig as LP_CFG
@@ -76,47 +76,49 @@ class TestFrame(wx.Frame):
         self._monitor.open('COM5', 9600, 8, 'N', 1)
         LP_PATH = Path(os.path.expanduser('~')) / 'Documents/LPTEST/LiverPerfusion/data/CDI Stream'
         self._monitor.open_stream(LP_PATH)
-        self._labels = {'Arterial pH': 'units', 'Arterial CO2': 'mmHg', 'Arterial O2': 'mmHg', 'Arterial Temperature': 'C', 'Arterial Bicarbonate': 'mmol/L', 'Arterial BE': 'mmol/L', 'K': 'mmol/L', 'O2 Saturation': '%', 'Hct': '%', 'Hb': 'g/dL'}
+        self._labels = {'Time': '', 'Arterial pH': 'units', 'Arterial pCO2': 'mmHg', 'Arterial pO2': 'mmHg', 'Arterial Temperature': 'C', 'Arterial Bicarbonate': 'mmol/L', 'Arterial BE': 'mmol/L', 'K': 'mmol/L', 'O2 Saturation': '%', 'Hct': '%', 'Hb': 'g/dL'}
 
         self.sizer_main = wx.BoxSizer(wx.HORIZONTAL)
 
         self.sizer_plot_grid = wx.GridSizer(cols=2, hgap=5, vgap=5)
-        self.sizer_plots = []
-        self._plots_main = []
-        self._plots_lt = []
+        self.sizer_plots = []  # List of sizers, one for each plot set (big + small)
+        self._plots_main = []  # List of TSMPanelPlotting objects; each associated w/a TSMSensorPlot object
+        self._plots_lt = []  # List of TSMPanelPlotLT objects; each associated w/a TSMSensorPlot object
 
-        for plot_name in ['pH', 'Venous O2 Saturation', 'Arterial pO2 (mmHg)', 'Arterial pCO2 (mmHg)']:
-            self.sizer_plots.append(self._add_lt(self._monitor))
+        for plot_name in ['Arterial pH', 'O2 Saturation', 'Arterial pO2', 'Arterial pCO2']:
+            self.sizer_plots.append(self._add_lt(plot_name))
 
         self.sizer_readout = wx.GridSizer(cols=1)
         self.sizer_config = wx.BoxSizer(wx.VERTICAL)
+        self.sizer_start_streams = wx.BoxSizer(wx.VERTICAL)
         self.choice_time = self._create_choice_time()
         self.label_choice_time = wx.StaticText(self, label='Display Window')
+        self.btn_stream_GB100 = wx.Button(self, label='Start Gas Mixer')
+        self.btn_stream_TSM = wx.Button(self, label='Start CDI Monitor')
+
+        self._mixer.start_stream()
 
         self.__do_layout()
         self.__set_bindings()
-
-        self._monitor.start_stream()
-        self._mixer.start_stream()
 
         for plot in self._plots_main:
             plot.show_legend()
 
         self.Bind(wx.EVT_CLOSE, self.OnClose)
 
-    def _add_lt(self, sensor):
+    def _add_lt(self, plot_name):
         sizer = wx.BoxSizer(wx.VERTICAL)
-        panel = PanelPlotting(self)
+
+        panel = TSMPanelPlotting(self)
         self._plots_main.append(panel)
-        plotraw = SensorPlot(sensor, panel.axes)
+        plotraw = TSMSensorPlot(plot_name, panel.axes)
         panel.add_plot(plotraw)
         sizer.Add(panel, 10, wx.ALL | wx.EXPAND, border=0)
 
-        panel = PanelPlotLT(self)
+        panel = TSMPanelPlotLT(self)
         self._plots_lt.append(panel)
         panel.plot_frame_ms = -1
-
-        plotraw = SensorPlot(sensor, panel.axes)
+        plotraw = TSMSensorPlot(plot_name, panel.axes)
         panel.add_plot(plotraw)
         sizer.Add(panel, 2, wx.ALL | wx.EXPAND, border=0)
 
@@ -145,8 +147,13 @@ class TestFrame(wx.Frame):
 
         self.sizer_config.Add(self.label_choice_time, 1, wx.ALL | wx.ALIGN_CENTER)
         self.sizer_config.Add(self.choice_time, 1, wx.ALL)
+        self.sizer_start_streams.Add(self.btn_stream_GB100, 1, wx.ALL)
+        self.sizer_start_streams.AddSpacer(5)
+        self.sizer_start_streams.Add(self.btn_stream_TSM, 1, wx.ALL)
         self.sizer_readout.AddSpacer(5)
         self.sizer_readout.Add(self.sizer_config, 1, wx.ALL)
+        self.sizer_readout.AddSpacer(5)
+        self.sizer_readout.Add(self.sizer_start_streams, 1, wx.ALL)
         self.sizer_main.Add(self.sizer_readout)
         self.SetSizer(self.sizer_main)
 
@@ -156,10 +163,11 @@ class TestFrame(wx.Frame):
 
         self.timer_update = wx.Timer(self)
         self.Bind(wx.EVT_TIMER, self.OnTimer)
-        self.timer_update.Start(6000, wx.TIMER_CONTINUOUS)
 
     def __set_bindings(self):
         self.choice_time.Bind(wx.EVT_CHOICE, self._onchange_plotchoice)
+        self.btn_stream_GB100.Bind(wx.EVT_TOGGLEBUTTON, self.OnGB100)
+        self.btn_stream_TSM.Bind(wx.EVT_TOGGLEBUTTON, self.OnTSM)
 
     def _onchange_plotchoice(self, event):
         self.__plot_frame = PlotFrame[self.choice_time.GetStringSelection()]
@@ -168,13 +176,38 @@ class TestFrame(wx.Frame):
 
     def OnTimer(self, event):
         if event.GetId() == self.timer_update.GetId():
-            self.update_value()
+            self.update_plots()
 
-    def update_value(self):
+    def OnGB100(self, event):
+        label = self.btn_stream_GB100.GetLabel()
+        if label == 'Start Gas Mixer':
+            self.btn_stream_GB100.SetLabel('Stop Gas Mixer')
+        elif label == 'Stop Gas Mixer':
+            self.btn_stream_GB100.SetLabel('Start Gas Mixer')
+
+    def OnTSM(self, event):
+        label = self.btn_stream_TSM.GetLabel()
+        if label == 'Start CDI Monitor':
+            self.btn_stream_TSM.SetLabel('Stop CDI Monitor')
+            self._monitor.start_stream()
+        elif label == 'Stop CDI Monitor':
+            self.btn_stream_TSM.SetLabel('Start CDI Monitor')
+        self.timer_update.Start(6000, wx.TIMER_CONTINUOUS)
+
+    def update_plots(self):
         data = self._monitor.get_parsed_data()
         data_list = list(data)
-        for num in range(0, 10):
+        time = data_list[0]
+        for num in range(1, 11):
             self.readouts[num].label_value.SetLabel(data_list[num])
+        self._plots_main[0].plot(data_list[1], time)
+        self._plots_main[1].plot(data_list[8], time)
+        self._plots_main[2].plot(data_list[3], time)
+        self._plots_main[3].plot(data_list[2], time)
+        self._plots_lt[0].plot(data_list[1], time)
+        self._plots_lt[1].plot(data_list[8], time)
+        self._plots_lt[2].plot(data_list[3], time)
+        self._plots_lt[3].plot(data_list[2], time)
 
     def OnClose(self, evt):
         self._monitor.stop_stream()
