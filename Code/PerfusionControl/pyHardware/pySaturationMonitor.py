3291cfbce8dc8e1f9de46930b10022fd81b5dc59
Allen Luna <aluna2313@gmail.com>
2022-03-23 18:13:21 -0400

    Updated TSM hardware code so that timestamps (in milliseconds from start) format are stored during data streaming

--- Code/PerfusionControl/pyHardware/pySaturationMonitor.py
+++ Code/PerfusionControl/pyHardware/pySaturationMonitor.py
@@ -4,6 +4,7 @@ import pathlib
 import datetime
 from time import perf_counter
 import numpy as np
+import struct
 from threading import Thread, Event
 
 DATA_VERSION = 4
@@ -43,7 +44,7 @@ class TSMSerial(USBSerial):
         self._end_of_header = 0
         self._last_idx = 0
         self._datapoints_per_ts = 1
-        self._bytes_per_ts = 101
+        self._bytes_per_ts = 105
 
         self.__thread_streaming = None
         self.__evt_halt_streaming = Event()
@@ -92,7 +93,7 @@ class TSMSerial(USBSerial):
         header = [f'File Format: {DATA_VERSION}',
                   f'Instrument: {self.name}',
                   f'Data Format: {str(np.dtype(np.byte))}',
-                  f'Sample Description: {self._datapoints_per_ts} (Each Sample includes Header, Time, Arterial pH, Arterial pCO2 (mmHg), Arterial pO2 (mmHg), Arterial Temperature (Celsius), Arterial HCO3- (mEq/L), Arterial Base Excess (mEq/L), Calculated O2 Sat, K (mmol/L), VO2 (Oxygen Consumption; ml/min), Pump Flow (L/min), BSA (m^2), Venous pH, Venous pCO2 (mmHg), Venous pO2 (mmHg), Venous Temperature (Celsius), Measured O2 Sat, Hct, Hb (g/dl))',
+                  f'Sample Description: {self._datapoints_per_ts} (Each Sample includes Timestamp(milliseconds from start, in byte format), Header, Time, Arterial pH, Arterial pCO2 (mmHg), Arterial pO2 (mmHg), Arterial Temperature (Celsius), Arterial HCO3- (mEq/L), Arterial Base Excess (mEq/L), Calculated O2 Sat, K (mmol/L), VO2 (Oxygen Consumption; ml/min), Pump Flow (L/min), BSA (m^2), Venous pH, Venous pCO2 (mmHg), Venous pO2 (mmHg), Venous Temperature (Celsius), Measured O2 Sat, Hct, Hb (g/dl))',
                   f'Bytes Per Sample: {self._bytes_per_ts}',
                   f'Start of Acquisition: {stamp_str, self._timestamp_perf}'
                   ]
@@ -116,9 +117,11 @@ class TSMSerial(USBSerial):
 
     def stream(self):
         if self._USBSerial__serial.inWaiting() > 0:
+            ts_bytes = struct.pack('i', int(perf_counter() * 1000.0))
             data_raw = self._USBSerial__serial.readline()
-            buf_len = len(data_raw)
-            self._write_to_file(data_raw)
+            data_final = ts_bytes + data_raw
+            buf_len = len(data_final)
+            self._write_to_file(data_final)
             self._last_idx += buf_len
             self._fid_write.flush()
             self._USBSerial__serial.flushInput()
@@ -141,8 +144,11 @@ class TSMSerial(USBSerial):
 
     def get_latest(self):
         _fid, data = self._open_read_latest()
-        string_data = str(data, 'ascii')[1:]
-        return string_data
+        time = data[:4]
+        ts, = struct.unpack('i', time)
+        values = data[4:]
+        string_data = str(values, 'ascii')[1:]
+        return ts, string_data
 
     def _open_read_latest(self):
         _fid = open(self.full_path, 'rb')
@@ -151,8 +157,7 @@ class TSMSerial(USBSerial):
         return _fid, data
 
     def get_parsed_data(self):
-        data = self.get_latest()
-        time = data[0:8]
+        time, data = self.get_latest()
         arterial_pH = data[9:13]
         arterial_CO2 = data[14:18]
         arterial_O2 = data[19:23]
