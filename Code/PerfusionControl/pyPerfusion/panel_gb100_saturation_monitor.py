a508d2100438f82ad205b8c0fce35fb569ca0ce3
Allen Luna <aluna2313@gmail.com>
2022-03-23 16:57:52 -0400

    Added and tested functionality for choosing initial gas mix and starting / stopping gas mixer

--- Code/PerfusionControl/pyPerfusion/panel_gb100_saturation_monitor.py
+++ Code/PerfusionControl/pyPerfusion/panel_gb100_saturation_monitor.py
@@ -90,13 +90,39 @@ class TestFrame(wx.Frame):
 
         self.sizer_readout = wx.GridSizer(cols=1)
         self.sizer_config = wx.BoxSizer(wx.VERTICAL)
+        self.sizer_gas_config = wx.BoxSizer(wx.VERTICAL)
+        self.sizer_balance = wx.BoxSizer(wx.HORIZONTAL)
+        self.sizer_gas1_choice = wx.BoxSizer(wx.HORIZONTAL)
+        self.sizer_gas1_percentage = wx.BoxSizer(wx.HORIZONTAL)
+        self.sizer_gas2_choice = wx.BoxSizer(wx.HORIZONTAL)
+        self.sizer_gas2_percentage = wx.BoxSizer(wx.HORIZONTAL)
+        self.sizer_flow = wx.BoxSizer(wx.HORIZONTAL)
         self.sizer_start_streams = wx.BoxSizer(wx.VERTICAL)
         self.choice_time = self._create_choice_time()
         self.label_choice_time = wx.StaticText(self, label='Display Window')
-        self.btn_stream_GB100 = wx.Button(self, label='Start Gas Mixer')
-        self.btn_stream_TSM = wx.Button(self, label='Start CDI Monitor')
 
-        self._mixer.start_stream()
+        parameters = ['Air', 'Nitrogen', 'Oxygen', 'Carbon Dioxide']
+        self.choice_gas1 = wx.Choice(self, choices=parameters)
+        self.choice_gas1.SetStringSelection(parameters[2])
+        self.choice_gas2 = wx.Choice(self, choices=parameters)
+        self.choice_gas2.SetStringSelection(parameters[1])
+
+        parameters = ['1', '2']
+        self.choice_balance = wx.Choice(self, choices=parameters)
+        self.choice_balance.SetStringSelection(parameters[0])
+
+        self.label_balance = wx.StaticText(self, label='Balance Channel:')
+        self.label_gas1 = wx.StaticText(self, label='Channel 1 Gas:')
+        self.label_gas1_percentage = wx.StaticText(self, label='Channel 1 Percentage:')
+        self.spin_gas1_percentage = wx.SpinCtrlDouble(self, min=0, max=100, initial=90, inc=1)
+        self.label_gas2 = wx.StaticText(self, label='Channel 2 Gas:')
+        self.label_gas2_percentage = wx.StaticText(self, label='Channel 2 Percentage:')
+        self.spin_gas2_percentage = wx.SpinCtrlDouble(self, min=0, max=100, initial=10, inc=1)
+        self.label_total_flow = wx.StaticText(self, label='Total Flow:')
+        self.spin_total_flow = wx.SpinCtrlDouble(self, min=0, max=400, initial=100, inc=1)
+
+        self.btn_stream_GB100 = wx.ToggleButton(self, label='Start Gas Mixer')
+        self.btn_stream_TSM = wx.ToggleButton(self, label='Start CDI Monitor')
 
         self.__do_layout()
         self.__set_bindings()
@@ -147,11 +173,40 @@ class TestFrame(wx.Frame):
 
         self.sizer_config.Add(self.label_choice_time, 1, wx.ALL | wx.ALIGN_CENTER)
         self.sizer_config.Add(self.choice_time, 1, wx.ALL)
+
+        self.sizer_balance.Add(self.label_balance)
+        self.sizer_balance.AddSpacer(3)
+        self.sizer_balance.Add(self.choice_balance)
+        self.sizer_gas1_choice.Add(self.label_gas1)
+        self.sizer_gas1_choice.AddSpacer(3)
+        self.sizer_gas1_choice.Add(self.choice_gas1)
+        self.sizer_gas1_percentage.Add(self.label_gas1_percentage)
+        self.sizer_gas1_percentage.AddSpacer(3)
+        self.sizer_gas1_percentage.Add(self.spin_gas1_percentage)
+        self.sizer_gas2_choice.Add(self.label_gas2)
+        self.sizer_gas2_choice.AddSpacer(3)
+        self.sizer_gas2_choice.Add(self.choice_gas2)
+        self.sizer_gas2_percentage.Add(self.label_gas2_percentage)
+        self.sizer_gas2_percentage.AddSpacer(3)
+        self.sizer_gas2_percentage.Add(self.spin_gas2_percentage)
+        self.sizer_flow.Add(self.label_total_flow)
+        self.sizer_flow.AddSpacer(3)
+        self.sizer_flow.Add(self.spin_total_flow)
+        self.sizer_gas_config.Add(self.sizer_balance)
+        self.sizer_gas_config.Add(self.sizer_gas1_choice)
+        self.sizer_gas_config.Add(self.sizer_gas1_percentage)
+        self.sizer_gas_config.Add(self.sizer_gas2_choice)
+        self.sizer_gas_config.Add(self.sizer_gas2_percentage)
+        self.sizer_gas_config.Add(self.sizer_flow)
+
         self.sizer_start_streams.Add(self.btn_stream_GB100, 1, wx.ALL)
         self.sizer_start_streams.AddSpacer(5)
         self.sizer_start_streams.Add(self.btn_stream_TSM, 1, wx.ALL)
+
         self.sizer_readout.AddSpacer(5)
         self.sizer_readout.Add(self.sizer_config, 1, wx.ALL)
+        self.sizer_readout.AddSpacer(5)
+        self.sizer_readout.Add(self.sizer_gas_config, 1, wx.ALL)
         self.sizer_readout.AddSpacer(5)
         self.sizer_readout.Add(self.sizer_start_streams, 1, wx.ALL)
         self.sizer_main.Add(self.sizer_readout)
@@ -174,25 +229,52 @@ class TestFrame(wx.Frame):
         for plot in self._plots_main:
             plot.plot_frame_ms = self.__plot_frame.value
 
-    def OnTimer(self, event):
-        if event.GetId() == self.timer_update.GetId():
-            self.update_plots()
-
     def OnGB100(self, event):
         label = self.btn_stream_GB100.GetLabel()
+        gas1 = self._mixer.get_gas_ID((self.choice_gas1.GetStringSelection()))
+        gas2 = self._mixer.get_gas_ID((self.choice_gas2.GetStringSelection()))
+        balance = float(self.choice_balance.GetStringSelection())
         if label == 'Start Gas Mixer':
             self.btn_stream_GB100.SetLabel('Stop Gas Mixer')
+            self._mixer.start_stream()
+            self.choice_gas1.Enable(False)
+            self.spin_gas1_percentage.Enable(False)
+            self.choice_gas2.Enable(False)
+            self.spin_gas2_percentage.Enable(False)
+            self.spin_total_flow.Enable(False)
+            self.choice_balance.Enable(False)
+            gas1_percentage = self.spin_gas1_percentage.GetValue()
+            gas2_percentage = self.spin_gas2_percentage.GetValue()
+            flow = int(self.spin_total_flow.GetValue())
+            self._mixer.change_gas_mix(gas1_percentage, gas2_percentage, flow, 1, gas1=gas1, gas2=gas2, balance_channel=balance)
         elif label == 'Stop Gas Mixer':
             self.btn_stream_GB100.SetLabel('Start Gas Mixer')
+            self._mixer.stop_stream()
+            self.choice_gas1.Enable(True)
+            self.spin_gas1_percentage.Enable(True)
+            self.choice_gas2.Enable(True)
+            self.spin_gas2_percentage.Enable(True)
+            self.spin_total_flow.Enable(True)
+            self.choice_balance.Enable(True)
+            gas1_percentage = self._mixer.get_channel_percent_value(1)
+            gas2_percentage = self._mixer.get_channel_percent_value(2)
+            flow = self._mixer.get_mainboard_total_flow()
+            self._mixer.change_gas_mix(gas1_percentage, gas2_percentage, flow, 0)
 
     def OnTSM(self, event):
         label = self.btn_stream_TSM.GetLabel()
         if label == 'Start CDI Monitor':
             self.btn_stream_TSM.SetLabel('Stop CDI Monitor')
             self._monitor.start_stream()
+            self.timer_update.Start(6000, wx.TIMER_CONTINUOUS)
         elif label == 'Stop CDI Monitor':
             self.btn_stream_TSM.SetLabel('Start CDI Monitor')
-        self.timer_update.Start(6000, wx.TIMER_CONTINUOUS)
+            self._monitor.stop_stream()
+            self.timer_update.Stop()
+
+    def OnTimer(self, event):
+        if event.GetId() == self.timer_update.GetId():
+            self.update_plots()
 
     def update_plots(self):
         data = self._monitor.get_parsed_data()
@@ -211,7 +293,9 @@ class TestFrame(wx.Frame):
 
     def OnClose(self, evt):
         self._monitor.stop_stream()
+        self._monitor.close_stream()
         self._mixer.stop_stream()
+        self._mixer.close_stream()
         for plot in self._plots_main:
             plot.Destroy()
         for plot in self._plots_lt:
