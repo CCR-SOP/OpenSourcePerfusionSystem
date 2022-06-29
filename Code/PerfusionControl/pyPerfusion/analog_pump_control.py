e5d4c1ec15e1b4d967a62653ad308eb2c5d279d3
Allen Luna <aluna2313@gmail.com>
2022-06-29 10:44:37 -0400

    Updated panel to eliminate hardcoding and instead load in pump info from config file

--- Code/PerfusionControl/pyPerfusion/analog_pump_control.py
+++ Code/PerfusionControl/pyPerfusion/analog_pump_control.py
@@ -6,55 +6,75 @@
 Basic code for controlling centrifugal and dialysis pumps via Analog Output
 """
 import wx
-
+import pyPerfusion.utils as utils
+import pyPerfusion.PerfusionConfig as LP_CFG
 from pyHardware.pyAO_NIDAQ import NIDAQ_AO
 from pyPerfusion.panel_AO import PanelAO
 
-devices = ['Hepatic Artery Centrifugal Pump', 'Portal Vein Centrifugal Pump', 'Perfusate Pump', 'Dialysate Pump']
+devices = ['Hepatic Artery Centrifugal Pump', 'Portal Vein Centrifugal Pump', 'Dialysis Blood Pump', 'Dialysate Inflow Pump', 'Dialysate Outflow Pump']
 
 
 class TestFrame(wx.Frame):
     def __init__(self, *args, **kwds):
         kwds["style"] = kwds.get("style", 0) | wx.DEFAULT_FRAME_STYLE
         wx.Frame.__init__(self, *args, **kwds)
-        sizer = wx.GridSizer(cols=2)
+        sizer = wx.GridSizer(cols=3)
 
         self._panel = []
         HA_panel = PanelAO(self, NIDAQ_AO(), devices[0])
-        HA_panel._panel_cfg.choice_dev.SetStringSelection('Dev3')
+        section = LP_CFG.get_hwcfg_section(devices[0])
+        dev = section['Device']
+        line = section['LineName']
+        HA_panel._panel_cfg.choice_dev.SetStringSelection(dev)
         HA_panel._panel_cfg.choice_dev.Enable(False)
-        HA_panel._panel_cfg.choice_line.SetStringSelection('1')
+        HA_panel._panel_cfg.choice_line.SetStringSelection(line)
         HA_panel._panel_cfg.choice_line.Enable(False)
         sizer.Add(HA_panel, 1, wx.ALL | wx.EXPAND, border=1)
         self._panel.append(HA_panel)
 
         PV_panel = PanelAO(self, NIDAQ_AO(), devices[1])
-        PV_panel._panel_cfg.choice_dev.SetStringSelection('Dev3')
+        section = LP_CFG.get_hwcfg_section(devices[1])
+        dev = section['Device']
+        line = section['LineName']
+        PV_panel._panel_cfg.choice_dev.SetStringSelection(dev)
         PV_panel._panel_cfg.choice_dev.Enable(False)
-        PV_panel._panel_cfg.choice_line.SetStringSelection('0')
+        PV_panel._panel_cfg.choice_line.SetStringSelection(line)
         PV_panel._panel_cfg.choice_line.Enable(False)
         sizer.Add(PV_panel, 1, wx.ALL | wx.EXPAND, border=1)
         self._panel.append(PV_panel)
 
         Perfusate_panel = PanelAO(self, NIDAQ_AO(), devices[2])
-        Perfusate_panel._panel_cfg.choice_dev.SetStringSelection('Dev2')
+        section = LP_CFG.get_hwcfg_section(devices[2])
+        dev = section['Device']
+        line = section['LineName']
+        Perfusate_panel._panel_cfg.choice_dev.SetStringSelection(dev)
         Perfusate_panel._panel_cfg.choice_dev.Enable(False)
-        Perfusate_panel._panel_cfg.choice_line.SetStringSelection('0')
+        Perfusate_panel._panel_cfg.choice_line.SetStringSelection(line)
         Perfusate_panel._panel_cfg.choice_line.Enable(False)
-        Perfusate_panel._panel_settings.lbl_offset.SetLabel('Desired Flow (ml/min)')
-        Perfusate_panel._panel_settings.spin_offset.SetMax(100)
         sizer.Add(Perfusate_panel, 1, wx.ALL | wx.EXPAND, border=1)
         self._panel.append(Perfusate_panel)
 
-        Dialysate_panel = PanelAO(self, NIDAQ_AO(), devices[3])
-        Dialysate_panel._panel_cfg.choice_dev.SetStringSelection('Dev2')
-        Dialysate_panel._panel_cfg.choice_dev.Enable(False)
-        Dialysate_panel._panel_cfg.choice_line.SetStringSelection('1')
-        Dialysate_panel._panel_cfg.choice_line.Enable(False)
-        Dialysate_panel._panel_settings.lbl_offset.SetLabel('Desired Flow (ml/min)')
-        Dialysate_panel._panel_settings.spin_offset.SetMax(100)
-        sizer.Add(Dialysate_panel, 1, wx.ALL | wx.EXPAND, border=1)
-        self._panel.append(Dialysate_panel)
+        Dialysate_inflow_panel = PanelAO(self, NIDAQ_AO(), devices[3])
+        section = LP_CFG.get_hwcfg_section(devices[3])
+        dev = section['Device']
+        line = section['LineName']
+        Dialysate_inflow_panel._panel_cfg.choice_dev.SetStringSelection(dev)
+        Dialysate_inflow_panel._panel_cfg.choice_dev.Enable(False)
+        Dialysate_inflow_panel._panel_cfg.choice_line.SetStringSelection(line)
+        Dialysate_inflow_panel._panel_cfg.choice_line.Enable(False)
+        sizer.Add(Dialysate_inflow_panel, 1, wx.ALL | wx.EXPAND, border=1)
+        self._panel.append(Dialysate_inflow_panel)
+
+        Dialysate_outflow_panel = PanelAO(self, NIDAQ_AO(), devices[4])
+        section = LP_CFG.get_hwcfg_section(devices[4])
+        dev = section['Device']
+        line = section['LineName']
+        Dialysate_outflow_panel._panel_cfg.choice_dev.SetStringSelection(dev)
+        Dialysate_outflow_panel._panel_cfg.choice_dev.Enable(False)
+        Dialysate_outflow_panel._panel_cfg.choice_line.SetStringSelection(line)
+        Dialysate_outflow_panel._panel_cfg.choice_line.Enable(False)
+        sizer.Add(Dialysate_outflow_panel, 1, wx.ALL | wx.EXPAND, border=1)
+        self._panel.append(Dialysate_outflow_panel)
 
         self.SetSizer(sizer)
         self.Fit()
@@ -74,6 +94,10 @@ class MyTestApp(wx.App):
         frame.Show()
         return True
 
+if __name__ == "__main__":
+    LP_CFG.set_base(basepath='~/Documents/LPTEST')
+    LP_CFG.update_stream_folder()
+    utils.setup_default_logging(filename='panel_gb100_saturation_monitor')
+    app = MyTestApp(0)
+    app.MainLoop()
 
-app = MyTestApp(0)
-app.MainLoop()
