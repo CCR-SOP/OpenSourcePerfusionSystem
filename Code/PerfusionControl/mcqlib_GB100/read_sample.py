8fd2fc5b6afafb2f372f2c7bf61a85da9b1a0d9f
Allen Luna <aluna2313@gmail.com>
2022-06-28 22:38:36 -0400

    Updated read_sample code for compatibility with past changes to underlying mcqlib_GB100/mcqlib/main code

--- Code/PerfusionControl/mcqlib_GB100/read_sample.py
+++ Code/PerfusionControl/mcqlib_GB100/read_sample.py
@@ -7,19 +7,21 @@ import mcqlib_GB100.mcqlib.main as mcq
 
 print("Script Start")
 
-print("fw ver", mcq.get_mainboard_fw_ver())
-print("hw ver", mcq.get_mainboard_hw_ver())
+mixer = mcq.Main('Arterial Gas Mixer')
+
+print("fw ver", mixer.get_mainboard_fw_ver())
+print("hw ver", mixer.get_mainboard_hw_ver())
 
 print("Reading Channels Values...")
 time.sleep(2)
 
-total_channels = mcq.get_total_channels()
+total_channels = mixer.get_total_channels()
 
 for channel in range(total_channels):
   channel_nr = channel + 1
 
-  idGas = mcq.get_channel_id_gas(channel_nr)
-  kFactor = mcq.get_channel_k_factor_gas(channel_nr)
+  idGas = mixer.get_channel_id_gas(channel_nr)
+  kFactor = mixer.get_channel_k_factor_gas(channel_nr)
   gasType = mcq.mcq_utils.get_gas_type(idGas)
 
   channel_info = "Channel {0} - Gas Id: {1}, Gas: {2}, K-Factor: {3}".format(channel_nr, idGas, gasType, kFactor)
