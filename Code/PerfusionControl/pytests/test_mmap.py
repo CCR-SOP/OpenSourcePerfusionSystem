c0c2d7af43f8842dab9cf9335a20e72fa0ee1d11
John Kakareka <kakareka@nih.gov>
2022-08-29 13:41:40 -0400

    remove redundant mmap example as replaced with unit testing add unit tests for numpy and mmap reads

--- Code/PerfusionControl/pytests/test_mmap.py
+++ Code/PerfusionControl/pytests/test_mmap.py
@@ -8,15 +8,26 @@ Author: John Kakareka
 """
 import pytest
 import mmap
-from pathlib import Path
+
+import numpy as np
+
 
 DUMMY_DATA = 'This is a test'.encode('utf-8')
+DUMMY_NP_DATA = np.arange(1, 11, 1, dtype=np.uint32)
 
-@pytest.fixture()
+@pytest.fixture
 def data_file(tmp_path_factory):
     fn = tmp_path_factory.mktemp('data') / 'test.dat'
     _fid = open(fn, 'wb')
     _fid.write(DUMMY_DATA)
+    _fid.close()
+    return fn
+
+@pytest.fixture
+def np_file(tmp_path_factory):
+    fn = tmp_path_factory.mktemp('data') / 'test_np.dat'
+    _fid = open(fn, 'wb')
+    _fid.write(DUMMY_NP_DATA.tobytes())
     _fid.close()
     return fn
 
@@ -85,3 +96,33 @@ def test_mmap_read_and_write_simultaneously(data_file):
     expected = DUMMY_DATA + new_data
     data = fid_read.read(5)
     assert data == expected[5:10]
+
+
+def test_mmap_np_read(np_file):
+    fid = open(np_file, 'rb')
+    mm = mmap.mmap(fid.fileno(), length=0, access=mmap.ACCESS_READ)
+    data = np.frombuffer(mm[:], dtype=np.uint32)
+    fid.close()
+
+    assert np.array_equal(data, DUMMY_NP_DATA)
+
+
+def test_mmap_np_write(np_file):
+    new_data = np.arange(11, 21, 1, dtype=np.uint32)
+    fid = open(np_file, 'r+b')
+    mm = mmap.mmap(fid.fileno(), length=0, access=mmap.ACCESS_WRITE)
+
+    mm_len = len(mm)
+    assert mm_len == len(DUMMY_NP_DATA.tobytes())
+
+    new_len = mm_len + len(new_data.tobytes())
+    mm.resize(new_len)
+    assert len(mm) == new_len
+
+    mm[mm_len:new_len] = new_data.tobytes()
+    fid.flush()
+
+    expected = np.concatenate((DUMMY_NP_DATA, new_data))
+    assert mm[:] == expected.tobytes()
+
+    fid.close()
