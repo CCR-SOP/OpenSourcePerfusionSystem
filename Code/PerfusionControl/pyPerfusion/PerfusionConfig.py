692c1d3cf7f85d59b6482ad8224cb545cdaeafe5
John Kakareka <kakareka@nih.gov>
2022-10-18 15:59:56 -0400

    move folder_management and PerfusionConfig from fixes/update_configuration_handling branch to this branch to separate out pyAI changes

--- Code/PerfusionControl/pyPerfusion/PerfusionConfig.py
+++ Code/PerfusionControl/pyPerfusion/PerfusionConfig.py
@@ -6,74 +6,106 @@ and under the public domain.
 
 Author: John Kakareka
 """
-import os
-from pathlib import Path
-from configparser import ConfigParser
-from datetime import datetime
 import logging
+from dataclasses import asdict
+
+from configparser import ConfigParser
+
+from pyPerfusion.folder_management import FolderManagement
 
 
-LP_PATH = {}
-LP_PATH['base'] = Path(os.path.expanduser('~')) / 'Documents/LiverPerfusion'
-LP_PATH['config'] = LP_PATH['base'] / 'config'
-LP_PATH['data'] = LP_PATH['base'] / 'data'
-LP_PATH['logs'] = LP_PATH['base'] / 'logs'
-LP_PATH['tmp'] = LP_PATH['base'] / 'tmp'
-LP_FILE = {}
-LP_FILE['hwcfg'] = LP_PATH['config'] / 'hardware.ini'
-LP_FILE['syringe'] = LP_PATH['config'] / 'syringe.ini'
-LP_FILE['receivers'] = LP_PATH['config'] / 'receivers.ini'
+# Set ACTIVE_CONFIG to either the StudyConfig or TestConfig objects
+# This helps prevent overwriting real study configurations during testing
+StudyConfig = FolderManagement('LiverPerfusion')
+TestConfig = FolderManagement('LPTest')
+ACTIVE_CONFIG = None
 
 
-def set_base(basepath='~/Documents'):
-    logging.getLogger(__name__).info(f'Setting configuration basepath to {basepath}')
-    global LP_PATH
-    base = Path(os.path.expanduser(basepath))
+def set_test_config():
+    global ACTIVE_CONFIG, TestConfig
+    ACTIVE_CONFIG = TestConfig
 
-    LP_PATH['base'] = base / 'LiverPerfusion'
-    LP_PATH['config'] = LP_PATH['base'] / 'config'
-    LP_PATH['data'] = LP_PATH['base'] / 'data'
-    LP_PATH['logs'] = LP_PATH['base'] / 'logs'
-    LP_PATH['tmp'] = LP_PATH['base'] / 'tmp'
 
-    LP_FILE['hwcfg'] = LP_PATH['config'] / 'hardware.ini'
-    LP_FILE['syringe'] = LP_PATH['config'] / 'syringe.ini'
-    LP_FILE['receivers'] = LP_PATH['config'] / 'receivers.ini'
+def set_study_config():
+    global ACTIVE_CONFIG, StudyConfig
+    ACTIVE_CONFIG = StudyConfig
 
-    for key in LP_PATH.keys():
-        logging.getLogger(__name__).info('Creating configuration folder structure')
-        LP_PATH[key].mkdir(parents=True, exist_ok=True)
 
-def update_hwcfg_section(name, updated_info):
-    config = ConfigParser()
-    config.optionxform = str
-    config.read(LP_FILE['hwcfg'])
-    if not config.has_section(name):
-        config.add_section(name)
-    config[name] = updated_info
-    with open(LP_FILE['hwcfg'], 'w') as file:
-        config.write(file)
+def get_date_folder():
+    global ACTIVE_CONFIG
+    return ACTIVE_CONFIG.get_folder('date')
 
-def get_hwcfg_section(name):
-    config = ConfigParser()
-    config.optionxform = str
+
+def get_cfg_filename(name: str):
+    global ACTIVE_CONFIG
+    return ACTIVE_CONFIG.get_folder('config') / f'{name}.ini'
+
+
+def hw_cfg_name():
+    global ACTIVE_CONFIG
+    return ACTIVE_CONFIG.get_folder('config') / 'hardware.ini'
+
+
+def receiver_cfg_name():
+    global ACTIVE_CONFIG
+    return ACTIVE_CONFIG.get_folder('config') / 'receiver.ini'
+
+
+def syringe_cfg_name():
+    global ACTIVE_CONFIG
+    return ACTIVE_CONFIG.get_folder('config') / 'syringe.ini'
+
+
+def write_section(cfg_name: str, section_name: str, info: dict):
+    global ACTIVE_CONFIG
+    parser = ConfigParser()
+    parser.optionxform = str
+    filename = get_cfg_filename(cfg_name)
+    if filename:
+        parser.read(filename)
+    if not parser.has_section(section_name):
+        parser.add_section(section_name)
+    parser[section_name] = info
+    with open(filename, 'w') as file:
+        parser.write(file)
+
+
+def read_section(cfg_name: str, section_name: str) -> dict:
+    global ACTIVE_CONFIG
+    parser = ConfigParser()
+    parser.optionxform = str
+    filename = get_cfg_filename(cfg_name)
+    if filename:
+        parser.read(filename)
     section = {}
-    config.read(LP_FILE['hwcfg'])
-    if config.has_section(name):
-        section = config[name]
+    if parser.has_section(section_name):
+        section = parser[section_name]
     return section
+
+
+def get_section_names(cfg_name: str):
+    global ACTIVE_CONFIG
+    parser = ConfigParser()
+    parser.optionxform = str
+    filename = get_cfg_filename(cfg_name)
+    sections = {}
+    if filename:
+        parser.read(filename)
+        sections = parser.sections()
+    return sections
 
 def open_receiver_info():
     config = ConfigParser()
-    config.read(LP_FILE['receivers'])
+    config.read(receiver_cfg_name())
     receiver_info = {}
     for key, val in config['Dexcom Receivers'].items():
         receiver_info[key] = val
     return receiver_info
 
+
 def save_syringe_info(codes, volumes):
     config = ConfigParser()
-    config.read(LP_FILE['syringe'])
+    config.read(syringe_cfg_name())
     if not config.has_section('Codes'):
         config.add_section('Codes')
     config['Codes'] = codes
@@ -86,27 +118,35 @@ def save_syringe_info(codes, volumes):
         volume[key] = str(val)[1:-1].replace("'", '')
     config['Volumes'] = volume
 
-    with open(LP_FILE['syringe'], 'w') as file:
+    with open(syringe_cfg_name(), 'w') as file:
         config.write(file)
 
-def open_syringe_info():
+
+def open_syringe_info(self):
     config = ConfigParser()
-    config.read(LP_FILE['syringe'])
+    config.read(syringe_cfg_name())
     volumes = {}
     for key, val in config['Volumes'].items():
         volumes[key] = val.split(', ')
     return config['Codes'], volumes
 
+
 def get_COMs_bauds():
     config = ConfigParser()
-    config.read(LP_FILE['syringe'])
+    config.read(syringe_cfg_name())
     COMs_bauds = {}
     for key, val in config['Syringes'].items():
         COMs_bauds[key] = val.split(', ')
     return COMs_bauds
 
-def update_stream_folder(base=''):
-    if not base:
-        base = datetime.now().strftime('%Y-%m-%d')
-    LP_PATH['stream'] = LP_PATH['data'] / base
-    LP_PATH['stream'].mkdir(parents=True, exist_ok=True)
+
+def setup_file_logger(lgr, level, filename=None):
+    global ACTIVE_CONFIG
+    if not filename:
+        filename = lgr.device_name
+    lgr.setLevel(level)
+    ch = logging.FileHandler(ACTIVE_CONFIG.get_folder('logs') / f'{filename}.log')
+    ch.setLevel(level)
+    formatter = logging.Formatter('%(asctime) s: %(name) s - %(levelname) s - %(message) s')
+    ch.setFormatter(formatter)
+    lgr.addHandler(ch)
