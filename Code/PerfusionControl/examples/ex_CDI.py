ec6266540ba835876480e2e2e885cf8b8197f32b
John Kakareka <kakareka@nih.gov>
2022-10-20 09:09:22 -0400

    move classes from example to pyCDI, minor code cleanups remove old pyCDI (no extension)

--- Code/PerfusionControl/examples/ex_CDI.py
+++ Code/PerfusionControl/examples/ex_CDI.py
@@ -1,72 +1,23 @@
-'''
+"""
 
 Example script to parse single set of packet mdoe data from CDI saturation monitor
 
 @project: Liver Perfusion, NIH
 @author: Stephie Lux, NIH
 
-'''
+"""
 
 import logging
-import serial
 
 import pyPerfusion.utils as utils
+import pyPerfusion.pyCDI as pyCDI
+
 utils.setup_stream_logger(logging.getLogger(), logging.DEBUG) # add in debugging comments
 
-class CDIStreaming:
-    def __init__(self):
-        self._logger = logging.getLogger(__name__)
-        self._comport = 'comport name, add later'
-        # should there be something indicating this is for packet mode instead of ASCII here?
-        self._baudrate = 9600
-        self._parity = None
-        self.__serial = serial.Serial()
-        # later add something to account for rate of reading and sampling
+COMPORT = 'COM1'
 
-    def open(self):
-        if self.__serial.is_open:
-            self.__serial.close()
-
-        self.__serial.port = self._comport
-        self.__serial.baudrate = self._baudrate
-        self.__serial.open()
-
-    # how many bytes are in one timestamp's worth of data in packet mode? does the read function even get the packet correctly? look into this more
-    def get_data(self, expected_bytes, timeout=0):
-        if self.__serial.is_open:
-            self.__serial.timeout = timeout
-            CDIPacket = self.__serial.read(expected_bytes)
-            return CDIPacket
-
-class CDIRawData:
-    def __init__ (self, response_str):
-        self.arterial_pH = float(response_str[9:13])
-        self.arterial_CO2 = float(response_str[14:18])
-        self.arterial_O2 = float(response_str[19:23])
-        self.arterial_temp = float(response_str[24:28])
-        self.arterial_bicarb = float(response_str[29:33])
-        self.arterial_BE = float(response_str[34:38])
-        self.calculated_O2_sat = float(response_str[39:43])
-        self.K = float(response_str[44:48])
-        self.VO2 = float(response_str[49:53])
-        self.Q = float(response_str[54:58])
-        self.BSA = float(response_str[59:63])
-        self.venous_pH = float(response_str[64:68])
-        self.venous_CO2 = float(response_str[69:73])
-        self.venous_O2 = float(response_str[74:78])
-        self.venous_temp = float(response_str[79:83])
-        self.measured_O2_sat = float(response_str[84:87])
-        self.hct = float(response_str[89:92])
-        self.hb = float(response_str[94:99])
-
-    # test ability to read all 3 sensors on CDI
-    def ExampleReturn(self):
-        print(f'Arterial pH is {self.arterial_pH}')
-        print(f'Venous pH is {self.venous_pH}')
-        print(f'Hemoglobin is {self.hb}')
-
-cdi = CDIStreaming()
+cdi = pyCDI.CDIStreaming('Test CDI')
 cdi.open()
-packet = cdi.get_data(1000) # temporary byte number
-data = CDIRawData(packet)
-data.ExampleReturn()
+packet = cdi.request_data()
+data = pyCDI.CDIRawData(packet)
+data.print_results()
--- Code/PerfusionControl/pyPerfusion/pyCDI.py
+++ Code/PerfusionControl/pyPerfusion/pyCDI.py
@@ -21,6 +21,35 @@ import pyPerfusion.PerfusionConfig as LP_CFG
 utils.setup_stream_logger(logging.getLogger(), logging.DEBUG)
 utils.configure_matplotlib_logging()
 
+
+class CDIRawData:
+    def __init__(self, response_str):
+        self.arterial_pH = float(response_str[9:13])
+        self.arterial_CO2 = float(response_str[14:18])
+        self.arterial_O2 = float(response_str[19:23])
+        self.arterial_temp = float(response_str[24:28])
+        self.arterial_bicarb = float(response_str[29:33])
+        self.arterial_BE = float(response_str[34:38])
+        self.calculated_O2_sat = float(response_str[39:43])
+        self.K = float(response_str[44:48])
+        self.VO2 = float(response_str[49:53])
+        self.Q = float(response_str[54:58])
+        self.BSA = float(response_str[59:63])
+        self.venous_pH = float(response_str[64:68])
+        self.venous_CO2 = float(response_str[69:73])
+        self.venous_O2 = float(response_str[74:78])
+        self.venous_temp = float(response_str[79:83])
+        self.measured_O2_sat = float(response_str[84:87])
+        self.hct = float(response_str[89:92])
+        self.hb = float(response_str[94:99])
+
+    # test ability to read all 3 sensors on CDI
+    def print_results(self):
+        print(f'Arterial pH is {self.arterial_pH}')
+        print(f'Venous pH is {self.venous_pH}')
+        print(f'Hemoglobin is {self.hb}')
+
+
 class CDIStreaming:
     def __init__(self, name): # find a good tutorial on this
         super().__init__()
@@ -30,7 +59,7 @@ class CDIStreaming:
         self.name = name
 
         self._serial = serial.Serial()
-        self.__addr = 0
+        self._baud = 9600
 
         self._queue = None
         self.__acq_start_t = None
@@ -42,50 +71,35 @@ class CDIStreaming:
     def is_open(self):
         return self._serial.is_open
 
-    def open(self, port_name: str, baud_rate: int, addr: int = 0) -> None: #how do we get port name, baudrate, etc.?
+    def open(self, port_name: str, baud_rate: int) -> None:
         if self._serial.is_open:
             self._serial.close()
 
         self._serial.port = port_name
         self._serial.baudrate = baud_rate
-        self._serial.xonxoff = True
+        self._serial.stopbits = serial.STOPBITS_ONE
+        self._serial.parity = serial.PARITY_NONE
+        self._serial.bytesize = serial.EIGHTBITS
+
         try:
             self._serial.open()
         except serial.serialutil.SerialException as e:
             self._lgr.error(f'Could not open serial port {self._serial.portstr}')
             self._lgr.error(f'Message: {e}')
-        self.__addr = addr
         self._queue = Queue()
-
-        self.send_wait4response('')
-        self.send_wait4response(f'address {self.__addr}\r')
-        self.send_wait4response('poll REMOTE\r')
 
     def close(self):
         if self._serial:
-            self.stop()
             self._serial.close()
 
-    def send_wait4response(self, str2send: str) -> str:
-        response = ''
-        if self._serial.is_open:
-            self._serial.write(str2send.encode('UTF-8'))
-            self._serial.flush()
-            response = ''
-            self._serial.timeout = 1.0
-            response = self._serial.read_until('\r', size=1000).decode('UTF-8')
-        # JWK, we should be checking error responses
-        # strip starting \r and ending \r
-        return response[1:-1]
+    # how many bytes are in one timestamp's worth of data in packet mode? does the read function even get the packet correctly? look into this more
+    def request_data(self, expected_bytes, timeout=0):
+        # send command to request data. see manual page c-5
+        # for packet mode, expected bytes should be a constant value
+        # so no need for this to be a parameter
+        CDIPacket = self.__serial.read(expected_bytes)
+        return CDIPacket
 
-    def stream_data(self): # this is probably wrong
-
-        self._serial.flushInput()
-        self._serial.flushOutput()
-        while True:
-            bytesToRead = self._serial.inWaiting()
-            data = self._serial.read(bytesToRead)
-            return data # need to make this a data frame
 ''' 
 methods
 
