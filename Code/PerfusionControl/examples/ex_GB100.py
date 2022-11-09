7c4fe1c1c6a94374ff9b1dec65015f1a0a133b58
stephielux <stephie.lux@nih.gov>
2022-11-09 13:06:54 -0500

    Updated ex_GB100 to change total flow, on/off status, and gas percentage

--- Code/PerfusionControl/examples/ex_GB100.py
+++ Code/PerfusionControl/examples/ex_GB100.py
@@ -4,6 +4,10 @@ import time
 mixer = mcq.Main('Arterial Gas Mixer')
 total_channels = mixer.get_total_channels()
 
+# mixer.set_working_status_ON()
+# mixer.set_working_status_OFF()
+
+mixer.set_mainboard_total_flow(100)
 totalFlow = mixer.get_mainboard_total_flow()
 print(f'Total flow is {totalFlow}')
 
@@ -13,6 +17,7 @@ for channel in range(total_channels):
     idGas = mixer.get_channel_id_gas(channel_nr)
     kFactor = mixer.get_channel_k_factor_gas(channel_nr)
     gasType = mcq.mcq_utils.get_gas_type(idGas)
+    mixer.set_channel_percent_value(channel_nr, 50)  # set to 50%
     channelPercentage = mixer.get_channel_percent_value(channel_nr)
     targetFlow = mixer.get_channel_target_sccm(channel_nr)
     actualFlow = mixer.get_channel_sccm_av(channel_nr)
