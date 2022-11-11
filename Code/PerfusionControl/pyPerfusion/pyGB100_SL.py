3e1dd0a4ececef4b97adfa1cc5e582cf65756799
aluna2313 <54429039+aluna2313@users.noreply.github.com>
2022-11-11 13:32:40 -0500

    Created new pyGB100_SL.py file to replace existign pyGB100.py file

---
+++ Code/PerfusionControl/pyPerfusion/pyGB100_SL.py
@@ -0,0 +1,15 @@
+"""Adjusts GB_100 gas mixers based on CDI output
+
+@project: LiverPerfusion NIH
+@author: Stephie Lux, NIH
+
+This work was created by an employee of the US Federal Gov
+and under the public domain.
+"""
+
+import pyPerfusion.pyCDI as pyCDI
+from pyPerfusion.FileStrategy import MultiVarToFile, MultiVarFromFile
+from pyPerfusion.SensorPoint import SensorPoint, ReadOnlySensorPoint
+import mcqlib_GB100.mcqlib.main as mcq
+import time
+
