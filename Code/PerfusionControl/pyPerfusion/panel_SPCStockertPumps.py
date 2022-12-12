f4f9c8a445402e4cb02840ee430903499e1a3835
stephielux <stephie.lux@nih.gov>
2022-12-12 16:09:24 -0500

    Added mock framework for sinusoidal pump control to panel_SPCStockertPump

--- Code/PerfusionControl/pyPerfusion/panel_SPCStockertPumps.py
+++ Code/PerfusionControl/pyPerfusion/panel_SPCStockertPumps.py
@@ -27,7 +27,7 @@ class StockertPumpPanel(wx.Panel):
         self.sizer = wx.StaticBoxSizer(static_box, wx.VERTICAL)
 
         self.label_pump_speed = wx.StaticText(self, label='Input pump speed (rpm):')
-        self.input_pump_speed = wx.TextCtrl(self, wx.ID_ANY, value='5')
+        self.input_pump_speed = wx.TextCtrl(self, wx.ID_ANY, value='100')
 
         self.start_btn = wx.ToggleButton(self, label='Start')
 
@@ -42,6 +42,57 @@ class StockertPumpPanel(wx.Panel):
         sizer_cfg.AddSpacer(2)
         sizer_cfg.Add(self.input_pump_speed, flags)
         sizer_cfg.AddSpacer(2)
+        sizer_cfg.Add(self.start_btn, flags)
+        sizer_cfg.AddSpacer(2)
+
+        self.sizer.SetSizeHints(self.parent)
+        self.SetSizer(self.sizer)
+        self.Layout()
+        self.Fit()
+
+    def __set_bindings(self):
+        self.start_btn.Bind(wx.EVT_TOGGLEBUTTON, self.OnStart)
+        # do we need something to accept the text input?
+
+    def OnStart(self, evt):
+        # write something to open
+        self.start_btn.SetLabel('Stop')
+
+class SinusoidalPumpPanel(wx.Panel):
+    def __init__(self, parent, **kwds):  # need to add in pump code
+        wx.Panel.__init__(self, parent, -1)
+        kwds["style"] = kwds.get("style", 0) | wx.DEFAULT_FRAME_STYLE
+        utils.setup_stream_logger(logging.getLogger(__name__), logging.DEBUG)
+        utils.configure_matplotlib_logging()
+        self.parent = parent
+
+        static_box = wx.StaticBox(self, wx.ID_ANY, label="Sinusoidal Pump")
+        self.sizer = wx.StaticBoxSizer(static_box, wx.VERTICAL)
+
+        self.label_trough_speed = wx.StaticText(self, label='Minimum speed (rpm):')
+        self.input_trough_speed = wx.TextCtrl(self, wx.ID_ANY, value='100')
+
+        self.label_peak_speed = wx.StaticText(self, label='Maximum speed (rpm):')
+        self.input_peak_speed = wx.TextCtrl(self, wx.ID_ANY, value='200')
+
+        self.start_btn = wx.ToggleButton(self, label='Start')
+
+        self.__do_layout()
+        self.__set_bindings()
+
+    def __do_layout(self):
+        flags = wx.SizerFlags().Border(wx.ALL, 5).Center()
+        sizer_cfg = wx.GridSizer(cols=4)
+
+        sizer_cfg.Add(self.label_trough_speed, flags)
+        sizer_cfg.AddSpacer(2)
+        sizer_cfg.Add(self.input_trough_speed, flags)
+        sizer_cfg.AddSpacer(2)
+        sizer_cfg.Add(self.label_peak_speed, flags)
+        sizer_cfg.AddSpacer(2)
+        sizer_cfg.Add(self.input_peak_speed, flags)
+        sizer_cfg.AddSpacer(2)
+
         sizer_cfg.Add(self.start_btn, flags)
         sizer_cfg.AddSpacer(2)
 
