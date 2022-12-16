9febc42c69f9c0f8755cc594c19922774bd2e762
stephielux <stephie.lux@nih.gov>
2022-12-16 10:51:45 -0500

    Updated pyGB100_SL to reflect sO2 instead ofg pO2. Corrected additional error case

--- Code/PerfusionControl/pyPerfusion/pyGB100_SL.py
+++ Code/PerfusionControl/pyPerfusion/pyGB100_SL.py
@@ -8,7 +8,6 @@ and under the public domain.
 """
 
 import logging
-import serial
 import time
 from datetime import datetime
 
@@ -16,14 +15,16 @@ import pyPerfusion.PerfusionConfig as PerfusionConfig
 import pyPerfusion.utils as utils
 import mcqlib_GB100.mcqlib.main as mcq
 
+PerfusionConfig.set_test_config()
+utils.setup_stream_logger(logging.getLogger(), logging.DEBUG)
+utils.configure_matplotlib_logging()
+
 # dictionary of acceptable value ranges
 physio_ranges = {'pH_lower': 7.3, 'pH_upper': 7.5,
                  'arterial_CO2_lower': 20, 'arterial_CO2_upper': 60,
-                 'arterial_O2_lower': 50, 'arterial_O2_upper': 300,
+                 'arterial_O2_lower_so2': 90, 'arterial_O2_upper_so2': 100,
                  'venous_CO2_lower': 20, 'venous_CO2_upper': 80,
-                 'venous_O2_lower': 20, 'venous_O2_upper': 150}
-
-# code to turn on both mixers - or do we need this if starting manually? --> probably put in script since method exists
+                 'venous_O2_lower_so2': 60, 'venous_O2_upper_so2': 92}
 
 class GB100_shift:
     def __init__(self, vessel, mixer):
@@ -32,19 +33,19 @@ class GB100_shift:
         if self.vessel == "HA":
             self.pH_index = 0
             self.CO2_index = 1
-            self.O2_index = 2
+            self.sO2_index = 4
             self.CO2_lower = physio_ranges['arterial_CO2_lower']
             self.CO2_upper = physio_ranges['arterial_CO2_upper']
-            self.O2_lower = physio_ranges['arterial_O2_lower']
-            self.O2_upper = physio_ranges['arterial_O2_upper']
+            self.O2_lower = physio_ranges['arterial_O2_lower_so2']
+            self.O2_upper = physio_ranges['arterial_O2_upper_so2']
         elif self.vessel == "PV":
             self.pH_index = 9
             self.CO2_index = 10
-            self.O2_index = 11
+            self.sO2_index = 13
             self.CO2_lower = physio_ranges['venous_CO2_lower']
             self.CO2_upper = physio_ranges['venous_CO2_upper']
-            self.O2_lower = physio_ranges['venous_O2_lower']
-            self.O2_upper = physio_ranges['venous_O2_upper']
+            self.O2_lower = physio_ranges['venous_O2_lower_so2']
+            self.O2_upper = physio_ranges['venous_O2_upper_so2']
 
         self.mixer = mixer
         self.co2_adjust = 5
@@ -78,9 +79,9 @@ class GB100_shift:
             new_percentage_mix = []
             percentage_mix = self.mixer.get_channel_percent_value(self.gas_dict['Carbon Dioxide'])
             if 0 < percentage_mix < 100:
-                if CDI_input[1] < self.CO2_lower:
+                if CDI_input[self.CO2_index] < self.CO2_lower:
                     new_percentage_mix = percentage_mix + self.co2_adjust
-                elif CDI_input[1] > self.CO2_upper:
+                elif CDI_input[self.CO2_index] > self.CO2_upper:
                     new_percentage_mix = percentage_mix - self.co2_adjust
                 self.mixer.set_channel_percent_value(self.gas_dict['Carbon Dioxide'], new_percentage_mix)
             else:
@@ -96,13 +97,12 @@ class GB100_shift:
         new_percentage_mix = []
         percentage_mix = self.mixer.get_channel_percent_value(self.gas_dict['Oxygen'])
         if 0 < percentage_mix < 100:
-            if CDI_input[2] < self.O2_lower:
+            if CDI_input[self.sO2_index] < self.O2_lower:
                 new_percentage_mix = percentage_mix + self.o2_adjust
-            elif CDI_input[2] > self.O2_upper:
+            elif CDI_input[self.sO2_index] > self.O2_upper:
                 new_percentage_mix = percentage_mix - self.o2_adjust
             self.mixer.set_channel_percent_value(self.gas_dict['Oxygen'], new_percentage_mix)
         else:
             self._logger.debug(f'Oxygen % is at {percentage_mix} and cannot be further changed')
-
 
 # continue working on error cases
