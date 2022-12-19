65ae8fe42ba5a84fdbb52be543797e6a78447eb5
stephielux <stephie.lux@nih.gov>
2022-12-19 10:00:03 -0500

    Corrected toggle buttons in panel_DialysisPumps and made skeleton functionality for dialysis pumps based on Allen's code

--- Code/PerfusionControl/pyPerfusion/panel_DialysisPumps.py
+++ Code/PerfusionControl/pyPerfusion/panel_DialysisPumps.py
@@ -13,9 +13,15 @@ import wx
 
 import pyPerfusion.PerfusionConfig as PerfusionConfig
 import pyPerfusion.utils as utils
+import time
 from pyPerfusion.pyDialysatePumps import DialysatePumps
 from pyHardware.pyAO_NIDAQ import NIDAQ_AO
 
+import pyPerfusion.pyCDI as pyCDI
+from pyPerfusion.SensorPoint import SensorPoint
+# from pyPerfusion.FileStrategy import MultiVarToFile  # not in this branch
+
+# add dict of limits
 
 class DialysisPumpPanel(wx.Panel):
     def __init__(self, parent, **kwds):  # add pump as an argument - just skeletonized right now
@@ -35,8 +41,9 @@ class DialysisPumpPanel(wx.Panel):
         self.label_outflow = wx.StaticText(self, label='Dialysate outflow pump flow rate (mL/min):')
         self.input_outflow_rate = wx.TextCtrl(self, wx.ID_ANY, value='5')
 
-        self.start_btn_inflow = wx.ToggleButton(self, label='Start')
-        self.start_btn_outflow = wx.ToggleButton(self, label='Start')
+        self.start_btn_inflow = wx.ToggleButton(self, label='Start Manual')
+        self.start_btn_outflow = wx.ToggleButton(self, label='Start Manual')
+        self.auto_start_btn = wx.ToggleButton(self, label='Start Automatic')
 
         self.__do_layout()
         self.__set_bindings()
@@ -53,6 +60,8 @@ class DialysisPumpPanel(wx.Panel):
         sizer_cfg.Add(self.input_outflow_rate, flags)
         sizer_cfg.Add(self.start_btn_outflow, flags)
 
+        sizer_cfg.Add(self.auto_start_btn, flags)
+
         self.sizer.Add(sizer_cfg)
 
         self.sizer.SetSizeHints(self.parent)
@@ -63,15 +72,45 @@ class DialysisPumpPanel(wx.Panel):
     def __set_bindings(self):
         self.start_btn_inflow.Bind(wx.EVT_TOGGLEBUTTON, self.OnStartInflow)
         self.start_btn_outflow.Bind(wx.EVT_TOGGLEBUTTON, self.OnStartOutflow)
+        self.auto_start_btn.Bind(wx.EVT_TOGGLEBUTTON, self.OnStartAuto)
         # do we need something to accept the text input?
 
     def OnStartInflow(self, evt):
         # write something to open
-        self.start_btn_inflow.SetLabel('Stop')
+        label = self.start_btn_inflow.GetLabel()
+        if label == "Start Manual":
+            self.start_btn_inflow.SetLabel('Stop Manual')
+        elif label == "Stop Manual":
+            self.start_btn_inflow.SetLabel('Start Manual')
 
     def OnStartOutflow(self, evt):
         # write something to open
-        self.start_btn_outflow.SetLabel('Stop')
+        label = self.start_btn_outflow.GetLabel()
+        if label == "Start Manual":
+            self.start_btn_outflow.SetLabel('Stop Manual')
+        elif label == "Stop Manual":
+            self.start_btn_outflow.SetLabel('Start Manual')
+
+    def OnStartAuto(self, evt):
+        label = self.auto_start_btn.GetLabel()
+        if label == "Start Automatic":
+            self.auto_start_btn.SetLabel('Stop Automatic')
+            # turn things on - Allen's OnDIalysis
+            self.AutomaticDialysis()
+        elif label == "Stop Automatic":
+            self.auto_start_btn.SetLabel('Start Automatic')
+            # turn things off - Allen's OnDialysis
+        # check limits of dialysis and that difference isn't too high- see dict - make sure all are true
+        # accept values from CDI - see panel_gas_mixers for example (but check with John before you do it)
+        # if K and hct are in range, don't run dialysis
+        # if either is out of range,  gentle dialysis
+        # be able to flip whether inflow or outflow is higher if trend gets reversed
+        # necessary hardware functioning to read everything in
+
+    def AutomaticDialysis(self):
+        time.sleep(5.0)
+        # pick initial rates for auto - 3 mL/min in and out? may end up changing this
+        # add functionality - UpdateDialysis methods
 
 class TestFrame(wx.Frame):
     def __init__(self, *args, **kwds):
