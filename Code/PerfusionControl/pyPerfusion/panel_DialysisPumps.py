a011c2210198719668e93aba50c051a0f0d13b64
stephielux <stephie.lux@nih.gov>
2023-03-09 10:34:13 -0500

    Preliminary CDI method added to panel_DialysisPumps

--- Code/PerfusionControl/pyPerfusion/panel_DialysisPumps.py
+++ Code/PerfusionControl/pyPerfusion/panel_DialysisPumps.py
@@ -47,6 +47,10 @@ class DialysisPumpPanel(wx.Panel):
         static_box = wx.StaticBox(self, wx.ID_ANY, label="Roller Pumps")
         self.sizer = wx.StaticBoxSizer(static_box, wx.HORIZONTAL)
 
+        self.automatic_start_btn = wx.ToggleButton(self, label='Start Automatic')
+
+        self.cdi_timer = wx.Timer(self)
+
         self.__do_layout()
         self.__set_bindings()
 
@@ -65,13 +69,22 @@ class DialysisPumpPanel(wx.Panel):
         self.sizer.Add(self._panel_outflow, flagsExpand)
         self.sizer.Add(self._panel_bloodflow, flagsExpand)
         self.sizer.Add(self._panel_glucose, flagsExpand)
+        self.sizer.Add(self.automatic_start_btn, flagsExpand)
 
         self.SetSizer(self.sizer)
         self.Layout()
         self.Fit()
 
     def __set_bindings(self):
-        pass
+        self.automatic_start_btn.Bind(wx.EVT_TOGGLEBUTTON, self.OnAutoStart)
+
+    def OnAutoStart(self, evt):
+        if self.automatic_start_btn.GetLabel() == "Start Automatic":
+            self.automatic_start_btn.SetLabel('Stop Automatic')
+            self.cdi_timer.Start(300_000, wx.TIMER_CONTINUOUS)
+        else:
+            self.automatic_start_btn.SetLabel('Stop Automatic')
+            self.cdi_timer.Stop()
 
 class TestFrame(wx.Frame):
     def __init__(self, *args, **kwds):
