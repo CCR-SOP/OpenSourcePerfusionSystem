4a985d33bbfa02b6f5291aa386c431415ce7bdf6
John Kakareka <kakareka@nih.gov>
2023-03-21 08:57:54 -0400

    update pyCDI to provide an easy way to convert variable names to an index. process string response in a CDIStreaming method replace CDIParsedData with new CDIData as data will always be provided as an array

--- Code/PerfusionControl/examples/ex_CDI_sensor.py
+++ Code/PerfusionControl/examples/ex_CDI_sensor.py
@@ -15,21 +15,22 @@ import pyPerfusion.PerfusionConfig as PerfusionConfig
 import pyPerfusion.utils as utils
 from pyPerfusion.Sensor import Sensor
 from pyHardware.SystemHardware import SYS_HW
+from pyPerfusion.pyCDI import CDIIndex, CDIData
 
 
 def main():
     SYS_HW.load_hardware_from_config()
-    # SYS_HW.load_mocks()
+    SYS_HW.load_mocks()
     SYS_HW.start()
 
-    sensor = Sensor(name='CDI')
+    sensor = Sensor(name='Mock CDI')
     sensor.read_config()
 
     sensor.start()
     reader = sensor.get_reader()
     print('Sleeping for 5 seconds to collect data')
     time.sleep(5)
-    cdi_var_index = 0  # arterial pH
+    cdi_var_index = CDIIndex.arterial_pH.value
     ts, last_samples = reader.retrieve_buffer(5000, 5, index=cdi_var_index)
     print(f'ts is {ts} and last_samples is {last_samples}')
     for ts, samples in zip(ts, last_samples):  # this didn't print anything
@@ -42,12 +43,19 @@ def main():
         ts, samples = reader.get_data_from_last_read(1)
         print(f'{ts}: sample is {samples}')
 
-    cdi_var_index = 9
+    cdi_var_index = CDIIndex.venous_O2.value
     print(f'Getting last acq, variable index {cdi_var_index}')
     ts, samples = reader.get_last_acq(index=cdi_var_index)
     print(f'{ts}: sample[{cdi_var_index}] is {samples}')
     print(f'{samples}')  # samples is an empty vector
 
+    print(f'Convert array of data into a CDIData object')
+    ts, all_vars = reader.get_last_acq()
+    cdi_data = CDIData(all_vars)
+    try:
+        print(f'arterial_temp is {cdi_data.arterial_temp}')
+    except AttributeError:
+        print('arterial_temp is not a valid attribute of CDIData')
     sensor.stop()
     SYS_HW.stop()
 
