1a2693df11344bb0df346c30cad717a0efac7598
John Kakareka <kakareka@nih.gov>
2023-03-30 16:01:53 -0400

    fix bug in stopping AutoGasMixer disable auto checkbox if flow is not enabled, stop autogasmixer when flow stopped

--- Code/PerfusionControl/pyPerfusion/pyAutoGasMixer.py
+++ Code/PerfusionControl/pyPerfusion/pyAutoGasMixer.py
@@ -10,23 +10,9 @@ and under the public domain.
 """
 import logging
 from threading import Thread, Event
-<<<<<<< HEAD
-<<<<<<< HEAD
+
 from time import sleep
 
-from pyPerfusion.utils import get_epoch_ms
-=======
-from time import sleep, time_ns
-
-
-from pyPerfusion.utils import get_epoch_ms
-import pyPerfusion.PerfusionConfig as PerfusionConfig
->>>>>>> dbaff70 (create new classes for autoadjusting gas mixers based on cdi inputs)
-=======
-from time import sleep
-
-from pyPerfusion.utils import get_epoch_ms
->>>>>>> 1bb2107 (integrate autogasmixer class into panel_gas_mixers)
 from pyPerfusion.pyCDI import CDIData
 
 
@@ -44,19 +30,14 @@ class AutoGasMixer:
         self.__thread = None
         self.is_streaming = False
 
-<<<<<<< HEAD
-<<<<<<< HEAD
-=======
->>>>>>> 1bb2107 (integrate autogasmixer class into panel_gas_mixers)
     @property
     def is_running(self):
         return self.is_streaming
 
     def run(self):
         self.is_streaming = True
-=======
+
     def run(self):
->>>>>>> dbaff70 (create new classes for autoadjusting gas mixers based on cdi inputs)
         next_t = self.acq_start_ms + self.adjust_rate_ms
         # sleep only 1 second so the thread can be terminated
         # in a quicker fashion. if adjust_rate is smaller, then use that
@@ -65,23 +46,14 @@ class AutoGasMixer:
             if get_epoch_ms() > next_t:
                 if self.gas_device and self.cdi_reader:
                     ts, all_vars = self.cdi_reader.get_last_acq()
-<<<<<<< HEAD
                     if all_vars is not None:
                         cdi_data = CDIData(all_vars)
-=======
-                    cdi_data = CDIData(all_vars)
-                    if cdi_data is not None:
->>>>>>> dbaff70 (create new classes for autoadjusting gas mixers based on cdi inputs)
                         self.update_gas_on_cdi(cdi_data)
                     else:
                         self._lgr.debug(f'{self.name} No CDI data. Cannot run gas mixers automatically')
                 next_t += self.adjust_rate_ms
             else:
-<<<<<<< HEAD
                 sleep(sleep_time/1_000)
-=======
-                sleep(sleep_time)
->>>>>>> dbaff70 (create new classes for autoadjusting gas mixers based on cdi inputs)
 
     def start(self):
         self.stop()
@@ -97,10 +69,7 @@ class AutoGasMixer:
         if self.is_streaming:
             self._event_halt.set()
             self.is_streaming = False
-<<<<<<< HEAD
             self._lgr.debug(f'AutoGasMixer {self.name} stopped')
-=======
->>>>>>> dbaff70 (create new classes for autoadjusting gas mixers based on cdi inputs)
 
     def update_gas_on_cdi(self, cdi_data):
         # this is the base class, so do nothing
@@ -112,28 +81,17 @@ class AutoGasMixerVenous(AutoGasMixer):
         super().__init__(name, gas_device, cdi_reader)
         self.o2_ch = 1
         self.co2_ch = 2
-<<<<<<< HEAD
-<<<<<<< HEAD
         self.o2_adjust = 2  # in %
 
     def update_gas_on_cdi(self, cdi_data):
         self._update_O2(cdi_data.venous_O2)
 
     def _update_O2(self, O2: float):
-=======
-=======
         self.o2_adjust = 2  # in %
->>>>>>> 1bb2107 (integrate autogasmixer class into panel_gas_mixers)
 
     def update_gas_on_cdi(self, cdi_data):
         self._update_O2(cdi_data.venous_O2)
 
-<<<<<<< HEAD
-    def update_O2(self, O2: float) -> float:
->>>>>>> dbaff70 (create new classes for autoadjusting gas mixers based on cdi inputs)
-=======
-    def _update_O2(self, O2: float):
->>>>>>> 1bb2107 (integrate autogasmixer class into panel_gas_mixers)
         o2_adjust = 0
         if O2 == -1:
             self._lgr.warning(f'{self.name}: O2 is out of range. Cannot be adjusted automatically')
@@ -154,8 +112,7 @@ class AutoGasMixerArterial(AutoGasMixer):
         super().__init__(name, gas_device, cdi_reader)
         self.o2_ch = 1
         self.co2_ch = 2
-<<<<<<< HEAD
-<<<<<<< HEAD
+
         self.co2_adjust = 1  # in %
         self.flow_adjust = 5  # in ml/min
 
@@ -176,11 +133,8 @@ class AutoGasMixerArterial(AutoGasMixer):
             self.gas_device.adjust_flow(-self.flow_adjust)
 
     def _update_CO2(self, pH: float, CO2: float):
-=======
-=======
         self.co2_adjust = 1  # in %
         self.flow_adjust = 5  # in ml/min
->>>>>>> 1bb2107 (integrate autogasmixer class into panel_gas_mixers)
 
     def update_gas_on_cdi(self, cdi_data):
         self._update_flow(cdi_data.arterial_pH)
@@ -194,12 +148,7 @@ class AutoGasMixerArterial(AutoGasMixer):
         elif pH > self.gas_device.cfg.pH_range[1]:
             self.gas_device.adjust_flow(-self.flow_adjust)
 
-<<<<<<< HEAD
     def update_CO2(self, pH: float, CO2: float) -> float:
->>>>>>> dbaff70 (create new classes for autoadjusting gas mixers based on cdi inputs)
-=======
-    def _update_CO2(self, pH: float, CO2: float):
->>>>>>> 1bb2107 (integrate autogasmixer class into panel_gas_mixers)
         co2_adjust = 0
         if pH == -1:
             self._lgr.warning(f'{self.name}: pH is out of range. Cannot be adjusted automatically')
