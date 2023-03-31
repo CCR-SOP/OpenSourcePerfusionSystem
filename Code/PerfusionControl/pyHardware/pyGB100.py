2872b42cd05aad0babbe54d4d6b525ca69fc9dbf
stephielux <stephie.lux@nih.gov>
2023-03-31 16:13:02 -0400

    Added sleeps to CDI/gas mixer automation

--- Code/PerfusionControl/pyHardware/pyGB100.py
+++ Code/PerfusionControl/pyHardware/pyGB100.py
@@ -132,7 +132,7 @@ class GasDevice:
         gas_type = 'NA'
         if self.hw is not None:
             addr = ChannelAddr[channel_num - 1] + ChannelRegisterOffsets['Id gas'].value
-            self._lgr.debug(f'addr is {addr}')
+            # self._lgr.debug(f'addr is {addr}')
             gas_id = self.hw.read_register(addr)
             try:
                 gas_type = GasNames(gas_id).name
