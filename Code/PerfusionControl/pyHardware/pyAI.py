346cf6566cc39c5f452025b196fa1b87b9154fcb
John Kakareka <kakareka@nih.gov>
2023-04-11 10:50:54 -0400

    refactor pyAI for consistent class usage (config has no name, object has name), remove obsolete code, change ai_channels from dict to list as channels maintains name so avoid a key name different from object name

--- Code/PerfusionControl/pyHardware/pyAI.py
+++ Code/PerfusionControl/pyHardware/pyAI.py
@@ -23,14 +23,13 @@ from threading import Thread, Event
 from queue import Queue, Empty
 from time import sleep
 import logging
-from dataclasses import dataclass, field
-from typing import List
+from dataclasses import dataclass
 
 import numpy as np
 import numpy.typing as npt
 
 import pyPerfusion.PerfusionConfig as PerfusionConfig
-from pyPerfusion.utils import get_epoch_ms
+import pyPerfusion.utils as utils
 
 
 class AIDeviceException(Exception):
@@ -39,7 +38,6 @@ class AIDeviceException(Exception):
 
 @dataclass
 class AIChannelConfig:
-    name: str = 'Channel'
     line: int = 0
     cal_pt1_target: np.float64 = 0.0
     cal_pt1_reading: np.float64 = 0.0
@@ -49,45 +47,40 @@ class AIChannelConfig:
 
 @dataclass
 class AIDeviceConfig:
-    name: str = ''
     device_name: str = ''
     sampling_period_ms: int = 0
     read_period_ms: int = 0
     data_type: npt.DTypeLike = np.dtype(np.float64).name
     buf_type: npt.DTypeLike = np.dtype(np.uint16).name
-    ch_names: List[str] = field(default_factory=list)
 
 
 class AIDevice:
-    def __init__(self):
-        self._lgr = logging.getLogger(__name__)
+    def __init__(self, name: str):
+        self.name = name
+        self._lgr = utils.get_object_logger(__name__, self.name)
+        self._lgr = logging.getLogger(f'{__name__}.{self.name}')
 
         self.__thread = None
         self._event_halt = Event()
-        # self._lock_buf = Lock()
 
         self.cfg = AIDeviceConfig()
-        self.ai_channels = {}
+        self.ai_channels = []
 
         self.acq_start_ms = 0
 
     def write_config(self):
-        PerfusionConfig.write_from_dataclass(self.cfg.name, 'General', self.cfg)
-        for ch in self.ai_channels.values():
-            PerfusionConfig.write_from_dataclass(self.cfg.name, ch.cfg.name, ch.cfg)
+        PerfusionConfig.write_from_dataclass(self.name, 'General', self.cfg)
+        for ch in self.ai_channels:
+            PerfusionConfig.write_from_dataclass(self.name, ch.name, ch.cfg)
 
     def read_config(self):
-        PerfusionConfig.read_into_dataclass(self.cfg.name, 'General', self.cfg)
-        channel_names = PerfusionConfig.get_section_names(self.cfg.name)
-        # delete the ch_names variable as this will be recreated as
-        # channels are re-added
-        self.cfg.ch_names = []
+        PerfusionConfig.read_into_dataclass(self.name, 'General', self.cfg)
+        channel_names = PerfusionConfig.get_section_names(self.name)
         for ch_name in channel_names:
             if ch_name != 'General':
-                ch_cfg = AIChannelConfig(name=ch_name)
-                self.add_channel(ch_cfg)
-                self.ai_channels[ch_name].read_config(ch_name)
-                self._lgr.info(f'read_config: added channel {ch_cfg}')
+                ch_cfg = AIChannelConfig()
+                self.add_channel(ch_name=ch_name, cfg=ch_cfg)
+                self._lgr.info(f'read_config: added channel {ch_name} with cfg: {ch_cfg}')
 
         self.open(self.cfg)
 
@@ -96,7 +89,7 @@ class AIDevice:
         """
         Creates a string as required by the hardware to define the analog input device and analog lines used
         """
-        lines = [ch.cfg.line for name, ch in self.ai_channels.items()]
+        lines = [ch.cfg.line for ch in self.ai_channels]
         if len(lines) == 0:
             dev_str = 'ai'
         else:
@@ -123,24 +116,27 @@ class AIDevice:
     def get_acq_start_ms(self):
         return self.acq_start_ms
 
-    def add_channel(self, cfg: AIChannelConfig):
-        if cfg.name in self.ai_channels.keys():
-            self._lgr.warning(f'Channel {cfg.name} already exists. Overwriting with new config')
-        else:
-            self.cfg.ch_names.append(cfg.name)
-        self.stop()
-        self.ai_channels[cfg.name] = AIChannel(cfg=cfg, device=self)
+    def channel_exists(self, ch_name: str):
+        ch_exists = any(ch.name == ch_name for ch in self.ai_channels)
+        return ch_exists
 
-    def remove_channel(self, name: str):
-        if name in self.ai_channels.keys():
-            self._lgr.info(f'Removing channel {name} from device {self.cfg.device_name}')
-            del self.ai_channels[name]
-            self.cfg.ch_names.remove(name)
+    def add_channel(self, ch_name: str, cfg: AIChannelConfig):
+        if self.channel_exists(ch_name):
+            self._lgr.warning(f'Channel {ch_name} already exists. Overwriting with new config')
+        self.stop()
+        ai = AIChannel(name=ch_name, cfg=cfg, device=self)
+        ai.read_config()
+        self.ai_channels.append(ai)
+
+    def remove_channel(self, ch_name: str):
+        if self.channel_exists(ch_name):
+            self._lgr.info(f'Removing channel {ch_name} from device {self.name}')
+            self.ai_channels = [ch for ch in self.ai_channels if ch.name == ch_name]
         else:
-            self._lgr.warning(f'Attempt to remove non-existent channel {name} from device {self.cfg.device_name}')
+            self._lgr.warning(f'Attempt to remove non-existent channel {ch_name} from device {self.name}')
 
     def remove_all_channels(self):
-        self.ai_channels = {}
+        self.ai_channels = []
 
     def open(self, cfg: AIDeviceConfig):
         self.cfg = cfg
@@ -151,10 +147,10 @@ class AIDevice:
     def start(self):
         self.stop()
         self._event_halt.clear()
-        self.acq_start_ms = get_epoch_ms()
+        self.acq_start_ms = utils.get_epoch_ms()
 
         self.__thread = Thread(target=self.run)
-        self.__thread.name = f'pyAI {self.cfg.name}'
+        self.__thread.name = f'pyAI {self.name}'
         self.__thread.start()
 
     def stop(self):
@@ -165,11 +161,11 @@ class AIDevice:
             self.__thread = None
 
     def run(self):
-        next_t = get_epoch_ms()
+        next_t = utils.get_epoch_ms()
         offset = 0
         while not self._event_halt.is_set():
             next_t += offset + self.cfg.read_period_ms
-            delay = next_t - get_epoch_ms()
+            delay = next_t - utils.get_epoch_ms()
             if delay > 0:
                 sleep(delay / 1_000.0)
                 offset = 0
@@ -178,27 +174,23 @@ class AIDevice:
             self._acq_samples()
 
     def _acq_samples(self):
-        buffer_t = get_epoch_ms()
-        for channel in self.ai_channels.values():
-            val = np.random.random_sample()  # * self._demo_amp[ch] + self._demo_offset[ch])
+        buffer_t = utils.get_epoch_ms()
+        for channel in self.ai_channels:
+            val = np.random.random_sample()
             buffer = np.ones(self.samples_per_read, dtype=self.cfg.data_type) * val
-            # self._lgr.debug(f'{self.cfg.name}: buffer_t = {buffer_t}')
             channel.put_data(buffer, buffer_t)
 
 
 class AIChannel:
-    def __init__(self, cfg: AIChannelConfig, device: AIDevice):
-        self._lgr = logging.getLogger(__name__)
+    def __init__(self, name: str, cfg: AIChannelConfig, device: AIDevice):
+        self.name = name
+        self._lgr = utils.get_object_logger(__name__, self.name)
         self.cfg = cfg
         self.device = device
 
         self._queue = Queue()
         self._q_timeout = 0.5
 
-        # parameters for randomly generated data when there is no underlying hardware
-        # used for testing and demo purposes
-        self._demo_amp = {}
-        self._demo_offset = {}
 
     @property
     def buf_len(self):
@@ -220,12 +212,12 @@ class AIChannel:
         return self.device.get_acq_start_ms()
 
     def write_config(self):
-        PerfusionConfig.write_from_dataclass(self.device.cfg.name, self.cfg.name, self.cfg)
+        PerfusionConfig.write_from_dataclass(self.device.name, self.name, self.cfg)
 
     def read_config(self, channel_name: str = None):
         if channel_name is None:
-            channel_name = self.cfg.name
-        PerfusionConfig.read_into_dataclass(self.device.cfg.name, channel_name, self.cfg)
+            channel_name = self.name
+        PerfusionConfig.read_into_dataclass(self.device.name, channel_name, self.cfg)
 
     def put_data(self, buf, t):
         data = self._calibrate(buf)
@@ -247,7 +239,6 @@ class AIChannel:
             self._queue.queue.clear()
 
     def _calibrate(self, buffer):
-
         if self.cfg.cal_pt2_reading - self.cfg.cal_pt1_reading == 0:
             data = buffer.astype(self.device.cfg.data_type)
         else:
