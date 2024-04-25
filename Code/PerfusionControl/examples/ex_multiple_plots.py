8ef3607b74f5150bc8c4094a151e77b4a875fc82
John Kakareka <kakareka@nih.gov>
2024-04-25 13:28:29 -0400

    cleanup example for multiple plots

--- Code/PerfusionControl/examples/ex_multiple_plots.py
+++ Code/PerfusionControl/examples/ex_multiple_plots.py
@@ -18,18 +18,20 @@ from pyPerfusion.PerfusionSystem import PerfusionSystem
 
 
 class TestFrame(wx.Frame):
-    def __init__(self, *args, **kwds):
+    def __init__(self, perfusion_system, *args, **kwds):
         kwds["style"] = kwds.get("style", 0) | wx.DEFAULT_FRAME_STYLE
         wx.Frame.__init__(self, *args, **kwds)
-        self._plots = []
 
-        sensors = [SYS_PERFUSION.get_sensor('Hepatic Artery Pressure'),
-                   SYS_PERFUSION.get_sensor('Portal Vein Flow')]
+        self.sys = perfusion_system
+        self._panels = []
+
+        sensors = [self.sys.get_sensor('Hepatic Artery Pressure'),
+                   self.sys.get_sensor('Portal Vein Flow')]
 
         sizer_plots = wx.GridSizer(cols=2)
         for idx, sensor in enumerate(sensors):
             panel = PanelPlotting(self)
-            self._plots.append(panel)
+            self._panels.append(panel)
             plot = SensorPlot(sensor, panel.axes, readout=True)
             plot.set_reader(sensor.get_reader())
             sizer_plots.Add(panel, 1, wx.ALL | wx.EXPAND, border=1)
@@ -43,38 +45,32 @@ class TestFrame(wx.Frame):
         self.Bind(wx.EVT_CLOSE, self.OnClose)
 
     def OnClose(self, evt):
-        for plot in self._plots:
-            plot.Destroy()
+        for panel in self._panels:
+            panel.Close()
+        for child in self.GetChildren():
+            child.Close()
         self.Destroy()
 
 
 class MyTestApp(wx.App):
     def OnInit(self):
-        frame = TestFrame(None, wx.ID_ANY, "")
+        frame = TestFrame(sys, None, wx.ID_ANY, "")
         self.SetTopWindow(frame)
         frame.Show()
-
         return True
 
 
 if __name__ == '__main__':
     PerfusionConfig.set_test_config()
     utils.setup_default_logging('ex_multiple_plots', logging.DEBUG)
-    SYS_PERFUSION = PerfusionSystem()
-    try:
-        SYS_PERFUSION.open()
-        SYS_PERFUSION.load_all()
-        SYS_PERFUSION.load_automations()
-    except Exception as e:
-        # if anything goes wrong loading the perfusion system
-        # close the hardware and exit the program
-        SYS_PERFUSION.close()
-        raise e
+    sys = PerfusionSystem()
+    sys.open()
+    sys.load_all()
 
     app = MyTestApp(0)
     app.MainLoop()
-
-    SYS_PERFUSION.close()
+    print('MyTestApp closed')
+    sys.close()
     for thread in threading.enumerate():
         print(thread.name)
 
