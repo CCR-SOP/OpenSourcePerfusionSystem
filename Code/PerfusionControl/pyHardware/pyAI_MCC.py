057080b36cfc8e8404f01f107b361b1af62ef999
John Kakareka <kakareka@nih.gov>
2024-04-26 10:42:46 -0400

    add additional error checking improve handling of buffers to avoid lost buffers fix bug in setting rate (was setting period, needed to specify samples/s)

--- Code/PerfusionControl/pyHardware/pyAI_MCC.py
+++ Code/PerfusionControl/pyHardware/pyAI_MCC.py
@@ -18,10 +18,11 @@ and under the public domain.
 from ctypes import cast, POINTER, c_double
 from dataclasses import dataclass
 import time
+import inspect
 
 import numpy as np
 from mcculw import ul
-from mcculw.enums import ScanOptions, ULRange, AnalogInputMode, FunctionType, Status, InterfaceType
+from mcculw.enums import ScanOptions, ULRange, FunctionType, Status, InterfaceType
 from mcculw.ul import ULError
 from mcculw.device_info import DaqDeviceInfo
 
@@ -33,7 +34,7 @@ import pyPerfusion.PerfusionConfig as PerfusionConfig
 @dataclass
 class AIMCCDeviceConfig(pyAI.AIDeviceConfig):
     ai_range: ULRange = ULRange.BIP10VOLTS
-    total_buffers: int = 10
+    total_buffers: int = 10  # require a minimum of 2 buffers, larger is better
 
 
 class MCCAIDevice(pyAI.AIDevice):
@@ -48,7 +49,7 @@ class MCCAIDevice(pyAI.AIDevice):
         self._acq_buf = None
         self._memhandle = None
         self._device = None
-        self._read_buf_idx = -1
+        self._read_buf_idx = 0
         self._chan_range = (-1, -1)
         self._scan_options = ScanOptions.BACKGROUND | ScanOptions.CONTINUOUS | ScanOptions.SCALEDATA
 
@@ -73,50 +74,69 @@ class MCCAIDevice(pyAI.AIDevice):
         return status_info
 
     def is_open(self):
-        # if channels were added and device name is valid, then we have
-        # confirmed that the device is present and the configuration
-        # is valid
         return self.board_num is not None
 
     def run(self):
+        # setup is done at the start of the run() as the UL library showed strange
+        # behavior when the setup was done in another function during early testing
+
+        first_run = True
         lines = [ch.cfg.line for ch in self.ai_channels]
         self._chan_range = (min(lines), max(lines))
+        self._read_buf_idx = 0
 
         total_count = self.acq_points * self.cfg.total_buffers
+        if self._memhandle is not None:
+            try:
+                ul.win_buf_free(self._memhandle)
+            except ULError as e:
+                self._lgr.exception(e)
+                raise pyAI.AIDeviceException(e)
+
         self._memhandle = ul.scaled_win_buf_alloc(total_count)
-        self._read_buf_idx = -1
-        if not self._memhandle:
-            self._lgr.error('Could not allocate memory')
+
+        if self._memhandle == 0:
+            raise pyAI.AIDeviceException(f'Could not allocate memory of size {total_count}')
+
         self._acq_buf = cast(self._memhandle, POINTER(c_double))
+        self._lgr.debug(f'period is {self.cfg.sampling_period_ms}')
+        rate = int(1000.0 / self.cfg.sampling_period_ms)
+        self._lgr.debug(f'rate is {rate}')
         try:
             actual_rate = ul.a_in_scan(self.board_num, self._chan_range[0], self._chan_range[1],
-                                       total_count, self.cfg.sampling_period_ms,
+                                       total_count, rate,
                                        self.cfg.ai_range, self._memhandle, self._scan_options)
-        except ULError as e:
-            self._lgr.error(e)
-        except Exception as e:
-            self._lgr.error(e)
-
-        try:
-            while not PerfusionConfig.MASTER_HALT.is_set():
-                period_timeout = self.cfg.read_period_ms / 1_000.0
-                if not self._event_halt.wait(timeout=period_timeout):
-                    if self.board_num is not None:
-                        status_info = self.get_status()
-                        if status_info.status == Status.RUNNING:
-                            if status_info.cur_index >= 0:
-                                buf_idx = int(status_info.cur_index / self.acq_points) - 1
-                                if not self._read_buf_idx == buf_idx:
-                                    if buf_idx > 0:
-                                        self._read_buf_idx = buf_idx - 1
-                                    else:
-                                        self._read_buf_idx = self.cfg.total_buffers - 1
-                                    self._acq_samples()
         except Exception as e:
             self._lgr.exception(e)
-        self._lgr.debug('run() ended')
+        if actual_rate != rate:
+            self._lgr.warning(f'Actual sampling rate for MCC Board {self.board_num} is {actual_rate}, should be {rate}')
+
+        while not PerfusionConfig.MASTER_HALT.is_set():
+            period_timeout = self.cfg.read_period_ms / 1_000.0 / 2.0
+            if not self._event_halt.wait(timeout=period_timeout):
+                try:
+                    if self.board_num is not None:
+
+                        status_info = self.get_status()
+                        if status_info.status == Status.RUNNING:
+
+                            if status_info.cur_index >= 0:
+                                buf_idx = int(status_info.cur_index / self.acq_points)
+                                if first_run:
+                                    if buf_idx == 0:
+                                        first_run = False
+                                else:
+                                    self._lgr.debug(f'buf_idx={buf_idx}, read_idx={self._read_buf_idx}, '
+                                                    f'cur_index={status_info.cur_index}, acq_pts={self.acq_points}')
+                                    if buf_idx != self._read_buf_idx:
+                                        self._acq_samples();
+                except Exception as e:
+                    raise pyAI.AIDeviceException(e)
+
+        self._lgr.debug('pyAI_MCC thread has ended.')
 
     def _acq_samples(self):
+        self._lgr.debug('acquiring samples')
         buffer_t = utils.get_epoch_ms() - self.get_acq_start_ms()
         try:
             offset = self._read_buf_idx * self.acq_points
@@ -124,19 +144,22 @@ class MCCAIDevice(pyAI.AIDevice):
                 ch_offset = offset + ch.cfg.line
                 buf = self._acq_buf[slice(ch_offset, ch_offset+self.acq_points, self.total_channels)]
                 ch.put_data(buf, buffer_t)
-
+            self._read_buf_idx += 1
+            if self._read_buf_idx == self.cfg.total_buffers:
+                self._read_buf_idx = 0
         except Exception as e:
             self._lgr.exception(f'For device {self.name}, unknown exception {e}')
 
     def remove_channel(self, name: str):
+        self._lgr.info(f'Removing channel {str}')
         acquiring = self.is_acquiring
-        self.stop()
+        if acquiring:
+            self.stop()
         super().remove_channel(name)
         if acquiring:
             self.start()
 
     def open(self):
-        self._lgr.debug('in open()')
         # ensure the buffer type is float64 for MCC devices
         self.cfg.buf_type = 'float64'
         super().open()
@@ -148,7 +171,6 @@ class MCCAIDevice(pyAI.AIDevice):
             self._lgr.info(f'Found multiple MCC devices, checking for matching board numbers')
             device = next(filter(lambda desc: ul.get_board_number(desc) == int(self.cfg.device_name), descriptors), None)
             self.board_num = self.cfg.device_number
-            self._lgr.debug(f'found device {device}')
         else:
             device = None
             self.board_num = None
@@ -161,19 +183,15 @@ class MCCAIDevice(pyAI.AIDevice):
             self.board_num = None
             raise pyAI.AIDeviceException(msg)
 
-        #self._device = ul.create_daq_device(self.board_num, device)
         self._device = DaqDeviceInfo(self.board_num)
-        self._lgr.debug(f'device={device}')
         self.board_num = ul.get_board_number(device)
-        self._lgr.debug(f'at end of open(), board_num={self.board_num}')
 
     def close(self):
-        self.stop()
+        if self.is_acquiring:
+            self.stop()
 
     def stop(self):
-        self._lgr.debug('stop() called')
         super().stop()
-        self._lgr.debug(f'in stop(), board_num is {self.board_num}')
         if self.board_num is not None:
             ul.stop_background(self.board_num, FunctionType.AIFUNCTION)
 
@@ -190,5 +208,4 @@ class MCCAIDevice(pyAI.AIDevice):
                 else:
                     self._lgr.debug('MCC Board stopped')
                     ul.win_buf_free(self._memhandle)
-        self._lgr.debug('stop ended')
 
